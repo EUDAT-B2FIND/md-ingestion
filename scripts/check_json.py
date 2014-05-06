e938f2082dfa90d0cb50f394713c233c88966236
John Mrziglod <mrzo@gmx.de>
2014-05-06 18:41:57 +0200

    + Add more validation routines

--- scripts/check_json.py
+++ scripts/check_json.py
@@ -1,4 +1,5 @@
-import optparse, os
+import optparse, os, sys
+import urllib2
 import simplejson as json
 
 def main():
@@ -11,40 +12,49 @@ def main():
 
     # find all files in directory:
     for root, dirs, files in os.walk(options.dir):
+        counter = 0
+        no_files = len(filter(lambda x: x.endswith(".json") , files))
+        if (no_files): print "\nProcessing dir: %s (%d files)" % (root, no_files)
         for file in files:
             if file.endswith(".json"):
-                 check_file(os.path.join(root, file))
+                counter += 1
+                update_progress(file,100*counter/no_files)
+                check_file(os.path.join(root, file))
+                 
+    print "\n%s" % ('-'*90)
                  
     for field in checklist:
-        length = len(checklist[field])
-        if length > 0:
-            print 'FIELD [%s] is missing in [%d] files' % (field, length)
-            
-            if (options.show):
-                limit = int(options.show_limit) if int(options.show_limit) < length else length-1
-                print "    "+ "\n    ".join(
-                    checklist[field][0:limit])
-            
-            # save results in file:
-            if (not os.path.exists('missing')):  os.makedirs('missing')
-            f = open('missing/main_%s' % field, 'w')
-            f.write("\n".join(checklist[field]))
-            f.close
+        for cmd in checklist[field]:
+            length = len(checklist[field][cmd])
+            if length > 0:
+                print 'ACTION [%s] failed in FIELD [%s] in [%d] files' % (cmd, field, length)
+                
+                if (options.show):
+                    limit = int(options.show_limit) if int(options.show_limit) < length else length-1
+                    print "    "+ "\n    ".join(
+                        checklist[field][cmd][0:limit])
+                
+                # save results in file:
+                if (not os.path.exists(options.outputdir)):  os.makedirs(options.outputdir)
+                f = open(options.outputdir+'/main_%s_%s' % (field,cmd), 'w')
+                f.write("\n".join(checklist[field][cmd]))
+                f.close
             
     for efield in checklist_extras:
-        length = len(checklist_extras[efield])
-        if length > 0:
-            print 'EXTRA FIELD [%s] is missing in [%d] files' % (efield, length)
-            if (options.show):
-                limit = int(options.show_limit) if int(options.show_limit) < length else length-1
-                print "    "+ "\n    ".join(
-                    checklist_extras[efield][0:limit])
-            
-            # save results in file:
-            if (not os.path.exists('missing')):  os.makedirs('missing')
-            f = open('missing/extra_%s' % efield, 'w')
-            f.write("\n".join(checklist_extras[efield]))
-            f.close
+        for cmd in checklist_extras[efield]:
+            length = len(checklist_extras[efield][cmd])
+            if length > 0:
+                print 'ACTION [%s] failed in EXTRA FIELD [%s] in [%d] files' % (cmd, efield, length)
+                if (options.show):
+                    limit = int(options.show_limit) if int(options.show_limit) < length else length-1
+                    print "    "+ "\n    ".join(
+                        checklist_extras[efield][cmd][0:limit])
+                
+                # save results in file:
+                if (not os.path.exists(options.outputdir)):  os.makedirs(options.outputdir)
+                f = open(options.outputdir+'/main_%s_%s' % (efield,cmd), 'w')
+                f.write("\n".join(checklist_extras[efield][cmd]))
+                f.close
 
 def check_file(file):
     jsondata = dict()
@@ -55,16 +65,28 @@ def check_file(file):
             print "[ERROR] Cannot load the json file! %s" % e
             
     for field in checklist:
-        if (not field in jsondata):
-            checklist[field].append(file)
+        for cmd in checklist[field]:
+            if (not command(cmd,field, jsondata)):
+                checklist[field][cmd].append(file)
     
-    extras_fields = list()
+    extras_fields = dict()
     for extra in jsondata['extras']:
-        extras_fields.append(extra['key'])
+        extras_fields[extra['key']] = extra['value']
     
     for efield in checklist_extras:
-        if (not efield in extras_fields):
-            checklist_extras[efield].append(file)
+        for cmd in checklist_extras[efield]:
+            if (not command(cmd, efield, extras_fields)):
+                checklist_extras[efield][cmd].append(file)
+    
+def command(cmd, field, dict, *other):
+    if (cmd == 'existence'):
+        return field in dict
+    elif(cmd == 'validation_url'):
+        try:
+            return urllib2.urlopen(dict[field], timeout=1).getcode() < 400
+        except:
+            return False
+    
             
 def load_checklists(file):
     try:
@@ -79,10 +101,19 @@ def load_checklists(file):
             elif ('[EXTRA]' in line):
                 category = 'extra'
             elif (not (line == '' or line.startswith('#'))):
+                # separate in field and commands:
+                field, commands = line.rstrip().split(':')
+
+                # separate the commands:
+                dico = {}
+                for cmd in  commands.split(","):
+                    dico[cmd] = []
+                
+                # generate the dictionary:
                 if category == 'main':
-                    list_main[line.rstrip()] = []
+                    list_main[field] = dico
                 else:
-                    list_extra[line.rstrip()] = []
+                    list_extra[field] = dico
     
         return list_main, list_extra
             
@@ -99,12 +130,16 @@ def get_options():
     )
    
     p.add_option('--dir', '-d',  help='\nDirectory where log, error and html-result files are stored. By default directory is created as startday/starthour/processid .', default=None)
-    p.add_option('--outputdir', '-o',  help='\nDirectory where log, error and html-result files are stored. By default directory is created as startday/starthour/processid .', default='missing')
+    p.add_option('--outputdir', '-o',  help='\nDirectory where log, error and html-result files are stored. By default directory is created as startday/starthour/processid .', default='failed')
     p.add_option('--checklist', '-c',  help='\nDirectory where log, error and html-result files are stored. By default directory is created as startday/starthour/processid .', default='scripts/checklist', metavar='FILE')
     p.add_option('--show', '-s',  help='\nShow failed files in terminal.', default=False, metavar='BOOLEAN')
     p.add_option('--show_limit',  help='\nThe limit of showed failed files in terminal.', default=20, metavar='INTEGER')
     
     return p.parse_args()
+    
+def update_progress(community,progress):
+    sys.stdout.write('\rProcessing file {0} : [{1}{2}] {3}%'.format(community,'#'*(progress/10), ' '*(10-progress/10), progress))
+    sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
