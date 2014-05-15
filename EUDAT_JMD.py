bbd8c5cfab7a83de8f82f0b90361eeaf73c45282
hwidmann <widmann@dkrz.de>
2014-05-15 17:16:41 +0200

    Set 'ignore deleted' again to True

--- EUDAT_JMD.py
+++ EUDAT_JMD.py
@@ -290,8 +290,8 @@ class HARVESTER(object):
     
         self.logger.info('    |   | %-4s | %-45s | %-45s |\n    |%s|' % ('#','OAI Identifier','DS Identifier',"-" * 103))
         try:
-            for record in sickle.ListRecords(**{'metadataPrefix':req['mdprefix'],'set':req['mdsubset'],'ignore_deleted':False,
-                'from':self.fromdate}):
+            for record in sickle.ListRecords(**{'metadataPrefix':req['mdprefix'],'set':req['mdsubset'],
+		'ignore_deleted':True,'from':self.fromdate}):
                 
                 stats['tcount'] += 1
 
