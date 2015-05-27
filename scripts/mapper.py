a589aba670acaaafe2b7278465281dbd1f9dba4b
hwidmann <widmann@dkrz.de>
2015-05-27 16:03:11 +0200

    Adapt mapper.py to new mapping design

--- scripts/mapper.py
+++ scripts/mapper.py
@@ -6,10 +6,11 @@ Licensed under AGPLv3.
 Moddifications :
   Heinrich Widmann @DKRZ 2014 : integrate actions in B2FIND.py
   Heinrich Widmann @DKRZ 2014 : rename (--> mapper.py) and add full mapping functionality to script
+  Heinrich Widmann @DKRZ 2015 : adapted to the new design of B2FIND mapper module
 
 HOW TO RUN
-$ ./mapper.py --mode MODE -i /path/to/input.[xml|json] -o /path/to/output.json -c /path/to/config.txt [-m /path/to/xpath-mapfile.xml 
-Input: xml or json file, depending on the this, total mapping (xml to json by JAVA XPATH2.0 converting + 'paostproc') or only 'postprocessing' (json to json) is performed
+$ ./mapper.py -i /path/to/input.[xml|json] -o /path/to/output.json [ -c /path/to/config.txt ] [ -m /path/to/xpath-mapfile.xml 
+Input (mandatory) : xml or json file, depending on the this, total mapping (xml to json by JAVA XPATH2.0 converting + 'paostproc') or only 'postprocessing' (json to json) is performed
 Configuration file (this is a text file, where actions or rules are specified, In the scripts folder, you can see the format of the config.txt)
 Map file (this is a xml file, where xml to json converting is defined by XPATH2.= rules)
 Output:json file (the output of the mapping is a json file ready to be validated and/or uploaded to CKAN)
@@ -34,6 +35,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 import B2FIND
+from B2FIND import CONVERTER as CV2
 import urllib, mimetypes
 import lxml.etree as etree
  
@@ -67,33 +69,84 @@ def get_conf(configFile):
 def save_data(dataset,dstFile):
     """
     saves a json file
-    """
-    with io.open(dstFile, 'w') as f: ## , encoding='utf-8') as f:
-        ##HEW??? json.dump(dataset,f, ensure_ascii=False)
-        ##encoded = dataset.encode('utf-8')
-        ## dataset=encoded.decode(encoding, 'ignore')
-        f.write(json.dumps(dataset, sort_keys = True, indent = 4,ensure_ascii=False))
+    """ ##HEW-D with io.open(dstFile, 'w') as f: ## , encoding='utf-8') as f:
+    with io.open(dstFile, 'w') as json_file:
+        try:
+            print '   | [INFO] decode json data'
+            data = json.dumps(dataset,sort_keys = True, indent = 4).decode('utf8')
+        except Exception as e:
+            print '    | [ERROR] %s : Cannot decode dataset %s' % (e,dataset)
+        try:
+            print '   | [INFO] save json file'
+            json_file.write(data)
+        except TypeError, err :
+            print '    | [ERROR] Cannot write json file %s : %s' % (path+'/json/'+filename,err)
+        except Exception as e:
+            print '    | [ERROR] %s : Cannot write json file %s' % (e,path+'/json/'+filename)
+
+        ##HEW-D f.write(json.dumps(dataset, sort_keys = True, indent = 4,ensure_ascii=False))
 
 def main():
-	parser = argparse.ArgumentParser(description='Postprocesses a json file')
+	parser = argparse.ArgumentParser(description='Perform B2FIND mapping')
         parser.add_argument('-v', '--verbose', action="count",
                         help="increase output verbosity (e.g., -vv is more than -v)", default=False)
-	parser.add_argument('-i','--srcFile',help='path to an input xml or json file to be mapped')
-	parser.add_argument('-c','--configFile',help='(optional) path to a configuration text file')
-	parser.add_argument('-m','--mapFile',help='path to a map file with xpath mapping rules (mandatory if srcFile is xml file)')
-	parser.add_argument('-o','--dstFile', help='path to output json file')
+        parser.add_argument('--mode', '-m', metavar='PROCESSINGMODE', help='\nThis specifies the performed mapping steps. Supported modes are (m)apping, (c)onverting, (p)ostprocessing, (x)path, (v)alidating, . Default is mode "-m", i.e. the total mapping workflow from (x)path selection up to generic python mapping', default='m')
+        parser.add_argument('inFile', metavar='INFILE',
+                   help='Path to the input file to be processed (could be a xml or json file)')
+        parser.add_argument('outFile', metavar='OUTFILE',
+                   help='Path to the output file (the mapped json file)')
+	parser.add_argument('--configFile', metavar='CONFIGFILE',help='path to the postprocessing configuration file (if not given tried to derivate from input file name)')
+	parser.add_argument('--mapFile',help='path to a map file with xpath mapping rules (if not given tried to derivate from input file name)')
 	parser.add_argument('-j','--jobdir', help='path to log dir', default='log')
 	
 	# parse command line arguments
 	args = parser.parse_args()
-	srcFile = args.srcFile
-	configFile = args.configFile
-	dstFile = args.dstFile
-	mapFile = args.mapFile
-	
-	if not (srcFile and dstFile):
-	    print parser.print_help()
+        if not args.inFile:   # if inffile is not given
+            parser.error('Input file is not given')
+ 	    print parser.print_help()
 	    exit(1)
+        else:
+            inFile = args.inFile
+        if not args.outFile:   # if outfilen is not given
+            parser.error('Output file is not given')
+ 	    print parser.print_help()
+	    exit(1)
+        else:
+            outFile = args.outFile
+
+        mapFileDir=os.getcwd()+'/mapfiles/'
+        oldmapFileDir='../mapper/current'
+        (inPath,inFileName)=os.path.split(inFile)
+        inPathList=inPath.split('/')
+        ext=os.path.splitext(inFileName)[1]
+        print 'ext %s' % ext
+        subset=inPathList[-2]
+        print 'subset %s' % subset
+        (comm,mdformat)=inPathList[-3].split('-')
+
+        if not args.configFile:   # if config file is not given
+            if mode != 'p':
+                configFile=mapFileDir+'mdpp_'+comm+'-'+mdformat+'.conf'
+        else:
+            configFile=os.path.abspath(args.configFile)
+
+        # check mapFile
+        if mode != 'x': # mapfile needed
+            if not args.mapFile:   # if map file is not given
+               mapFile=mapFileDir+comm+'-'+mdformat+'.xml'
+            else:
+               mapFile=args.mapFile
+
+            if os.path.isfile(mapFile):
+               mapFile=os.path.abspath(mapFile)
+            else:
+               print '[ERROR] Can not access mapfile %s !' % mapFile
+               exit(1)
+
+            print ' |- Mapfile\t%s' % mapFile
+
+
+
 
         # make jobdir
         now = time.strftime("%Y-%m-%d %H:%M:%S")
@@ -104,17 +157,16 @@ def main():
         OUT = B2FIND.OUTPUT(pstat,now,jid,args)
 
         # create CONVERTER object:
-        CV = B2FIND.CONVERTER(OUT,sys.path[0]+'/../../mapper/current')
+        CV = B2FIND.CONVERTER(OUT)
 	
 	# read input and config files
- 	dataset = get_dataset(srcFile)
- 	##print 'mode %s' % mode
+ 	dataset = get_dataset(inFile)
 
         global MapperVersion
         MapperVersion = '0.1'
         CV.logger.info('\nVersion:  \t%s' % MapperVersion)
         if mode == 1: 
-          modetext='JAVA XPATH xml2json converting + Python json2json mapping' 
+            modetext='JAVA XPATH xml2json converting + Python json2json mapping' 
         else : 
             modetext='Python json2json mapping'
         CV.logger.info('Run mode:   \t%s' % modetext)
@@ -122,29 +174,20 @@ def main():
         CV.logger.info('')
 
         if mode == 1 :
-            # check mapfile
-            if not (mapFile):
-               print parser.print_help()
-	       exit(1)
-            if not os.path.isfile(mapFile):
-               self.logger.error('[ERROR] Mapfile %s does not exist !' % mapfile)
-               exit(1)
-
             if (not os.path.isdir('tmpdir/xml')):
                os.makedirs('tmpdir/xml')
-            shutil.copy2(srcFile, 'tmpdir/xml')
-            xmlfile=os.path.basename(srcFile)
-            ##inpath=os.path.dirname(srcFile)
+            shutil.copy2(inFile, 'tmpdir/xml')
+            xmlfile=os.path.basename(inFile)
+            ##inpath=os.path.dirname(inFile)
             inpath=os.path.abspath('tmpdir/xml')
             outpath=os.getcwd()+'/tmpdir'
-            root='../mapper/current'
-            cp = ".:"+":".join(filter(lambda x: x.endswith('.jar'), os.listdir(root+'/lib')))
-            program = (filter(lambda x: x.endswith('.jar') and x.startswith('md-mapper-'), os.listdir(root)))[0]
+            cp = ".:"+":".join(filter(lambda x: x.endswith('.jar'), os.listdir(oldmapFileDir+'/lib')))
+            program = (filter(lambda x: x.endswith('.jar') and x.startswith('md-mapper-'), os.listdir(oldmapFileDir)))[0]
             # run the converter
-            CV.logger.debug(' Inpath\t%s\nJava root\t%s\nJava Class Path\t%s\nJAVA program\t%s' % (inpath,root,cp,program))
+            CV.logger.debug(' Inpath\t%s\nJava oldmapFileDir\t%s\nJava Class Path\t%s\nJAVA program\t%s' % (inpath,oldmapFileDir,cp,program))
             proc = subprocess.Popen(
                ["cd '%s'; java -cp lib/%s -jar %s inputdir=%s outputdir=%s mapfile=%s"% (
-                 os.getcwd()+'/'+root, cp,program,inpath,outpath, mapFile
+                 os.getcwd()+'/'+oldmapFileDir, cp,program,inpath,outpath, mapFile
                )], stdout=subprocess.PIPE, shell=True)
             (out, err) = proc.communicate()
             shutil.rmtree('tmpdir/xml')
@@ -164,15 +207,11 @@ def main():
                     except:
                       print '    | [ERROR] Cannot load json file %s' % jsonfile
         else:
-           jsonfile=srcFile
+           jsonfile=inFile
 
- 	
-        
-        print 'conffile %s' % configFile
         if configFile :
-          print 'xxx'
           if not os.path.isfile(configFile) :
-             self.logger.info('[INFO] Can not access config file %s => no postprocessing excecuted !' % configFile)
+             CV.logger.info('[INFO] Can not access config file %s => no postprocessing excecuted !' % configFile)
           else:
              try:
                ## md postprocessing
@@ -182,29 +221,59 @@ def main():
              except:
                CV.logger.error('    | [ERROR] during postprocessing ')
         else:
-          CV.logger.info('[INFO] No config file given => no postprocessing excecuted !')
+          CV.logger.info('[INFO] No config file given => rule specific postprocessing skipped !')
 
+        disctab = CV.cv_disciplines()
+
+        publdate=None
         # loop over all fields
         for facet in dataset:
-            if facet == 'extras':
-               try:   ### Semantic mapping of extra fields
+              if facet == 'author':
+                dataset[facet] = CV.cut(dataset[facet],'\(\d\d\d\d\)',1).strip()
+                dataset[facet] = CV.remove_duplicates(dataset[facet])
+              elif facet == 'tags':
+                dataset[facet] = CV.list2dictlist(dataset[facet]," ")
+              elif facet == 'url':
+                iddict = CV.map_identifiers(dataset[facet])
+              elif facet == 'extras':
+                try:   ### Semantic mapping of extra fields
                   for extra in dataset[facet]:
-                       if extra['key'] == 'Language':
-                           # generic mapping of languages
-                           extra['value'] = CV.map_lang(extra['value'])
-                       if extra['key'] == 'Discipline':
-                           # generic mapping of discipline
-                           disctab = CV.cv_diciplines()
-                           extra['value'] = CV.map_discipl(extra['value'],disctab.discipl_list)           # generic mapping of languages
-               except:
-                   CV.logger.error('    | [ERROR] during mapping of %s ' % extra['key'])
- 	
-            elif isinstance(dataset[facet], basestring) :
-               ### mapping of default string fields
-               dataset[facet]=dataset[facet].encode('ascii', 'ignore')
-             	
+                    if type(extra['value']) is list:
+                      extra['value']=CV.uniq(extra['value'])
+                      if len(extra['value']) == 1:
+                        extra['value']=extra['value'][0] 
+                    elif extra['key'] == 'Discipline':
+                      extra['value'] = CV.map_discipl(extra['value'],disctab.discipl_list)
+                    elif extra['key'] == 'Publisher':
+                      extra['value'] = CV.cut(extra['value'],'=',2)
+                      extra['value'] = CV.remove_duplicates(extra['value'])
+                    elif extra['key'] == 'SpatialCoverage':
+                      desc,slat,wlon,nlat,elon=CV.map_spatial(extra['value'])
+                      if wlon and slat and elon and nlat :
+                        spvalue="{\"type\":\"Polygon\",\"coordinates\":[[[%s,%s],[%s,%s],[%s,%s],[%s,%s],[%s,%s]]]}" % (wlon,slat,wlon,nlat,elon,nlat,elon,slat,wlon,slat)
+                      if desc :
+                        extra['value']=desc
+                    elif extra['key'] == 'TemporalCoverage':
+                      desc,stime,etime=CV.map_temporal(extra['value'])
+                      if desc:
+                        extra['value']=desc
+                    elif extra['key'] == 'Language':
+                      extra['value'] = CV.map_lang(extra['value'])
+                    elif extra['key'] == 'PublicationYear':
+                      publdate=CV.date2UTC(extra['value'])
+                      extra['value'] = CV.cut(extra['value'],'\d\d\d\d',0)
+                    elif extra['key'] == 'Contact':
+                      extra['value'] = CV.remove_duplicates(extra['value'])
+                    if type(extra['value']) is not str and type(extra['value']) is not unicode :
+                      CV.logger.debug(' [INFO] value of key %s has type %s : %s' % (extra['key'],type(extra['value']),extra['value']))
+                except Exception as e:
+                  CV.logger.info('    | [WARNING] %s : during mapping of field %s with value %s' % (e,extra['key'],extra['value']))
+                  continue
+        if publdate :
+                dataset['extras'].append({"key" : "PublicationTimestamp", "value" : publdate }) 
+
  	# save output json to file
-	save_data(dataset,dstFile)
+	save_data(dataset,outFile)
 
 if __name__ == "__main__":
 	main()
