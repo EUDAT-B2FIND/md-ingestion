ff5b4a7a9e6103cf6c9bf7952cd8cc8ce6ae829d
hwidmann <widmann@dkrz.de>
2018-01-02 14:59:53 +0100

    Optional argument 'targethost' added

--- cron_ingest.sh
+++ cron_ingest.sh
@@ -2,9 +2,23 @@
 
 ## Cronjob for B2FIND ingestion
 
-community=$1
-##daysago=$2
+# check arguments
+if [[ $# -lt 2 ]];
+then
+    echo "Missing arguments:/n Syntax: cron_test.sh community fromdays [targethost]"
+    exit -1
+else
+    community=$1
+    daysago=$2
+fi
+if [[ -z $3 ]];
+then
+    targethost='eudat-b1.dkrz.de:8080'
+else
+    targethost=$3
+fi
 
+# set work directory
 if [[ $(hostname) == centos* ]]; 
 then
     WORK="${HOME}/Projects/EUDAT/EUDAT-B2FIND/md-ingestion"
@@ -13,10 +27,10 @@ else
 fi
     
 TODAY=`date +\%F`
-NDAYSAGO="$2 days ago"
+NDAYSAGO="${daysago} days ago"
 YESTERDAY=`date -d "$NDAYSAGO" '+%Y-%m-%d'`
 cd $WORK
 set -x
-./manager.py -c $community -i eudat-b1.dkrz.de:8080 --handle_check=credentials_11098 --fromdate $YESTERDAY >log/ingest_${community}_${TODAY}.out 2>log/ingest_${community}_${TODAY}.err
+./manager.py -c $community -i $targethost --handle_check=credentials_11098 --fromdate $YESTERDAY >log/ingest_${community}_${TODAY}.out 2>log/ingest_${community}_${TODAY}.err
 
 exit 0
