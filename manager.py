64c1837e5e4b0be5cb4c379fe2dfdb509ff4d583
seolih <martens@dkrz.de>
2018-06-05 15:46:49 +0200

    Upgrade to B2FIND version 2.4.0

--- manager.py
+++ manager.py
@@ -53,7 +53,7 @@ def main():
 
     # check the version from svn:
     global ManagerVersion
-    ManagerVersion = '2.3.1'
+    ManagerVersion = '2.4.0'
 
     # parse command line options and arguments:
     modes=['h','harvest','c','convert','m','map','v','validate','o','oaiconvert','u','upload','h-c','c-u','h-u', 'h-d', 'd','delete']
