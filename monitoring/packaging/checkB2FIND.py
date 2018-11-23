0299bfa2db3e01b0ea2ff5a9f11ade8ed4b8416e
Heiner Widmann <widmann@dkrz.de>
2018-11-23 14:32:03 +0100

    Bugfixes for CKAN requests and adoption to Python3

--- monitoring/packaging/checkB2FIND.py
+++ monitoring/packaging/checkB2FIND.py
@@ -19,8 +19,19 @@ import traceback
 import os, sys, io, time
 import argparse
 import timeout_decorator
-import urllib2, urllib, socket
-import json, pprint
+PY2 = sys.version_info[0] == 2
+if PY2:
+    from urllib import quote
+    from urllib2 import urlopen, Request
+    from urllib2 import HTTPError,URLError
+else:
+    from urllib import parse
+    from urllib.request import urlopen, Request
+    from urllib.error import HTTPError,URLError
+
+
+import socket
+import simplejson as json
 
 
 def check_url(url):
@@ -38,12 +49,12 @@ def check_url(url):
     resplen=''
     try:
         start=time.time()
-        if (urllib2.urlopen(url, timeout=1).getcode() < 501):
+        if (urlopen(url, timeout=1).getcode() < 501):
             msg='[OK]'
             retcode=0
         rta=time.time()-start
         
-    except urllib2.URLError as e:
+    except URLError as e:
         ## msg="    %s and %s" % (e,traceback.format_exc())
         msg="    [URLError] %s" % e
         retcode = 2    #catched
@@ -65,9 +76,6 @@ def check_url(url):
 
     return (retcode,msg,resplen,rta)
 
-
-
-
 def check_ckan_action(actionreq,data,rows):
     # Checks and validates a request or action submitted to CKAN
     #
@@ -83,41 +91,31 @@ def check_ckan_action(actionreq,data,rows):
     rta=0
     try:
         start=time.time()
-        request = urllib2.Request(actionreq)
-        if data :
-            data_string = urllib.quote(json.dumps(data))
-            response = urllib2.urlopen(request,data_string)
-        else :
-            response = urllib2.urlopen(request)
+        encoding='utf8'
+        ## print('actionreq %s' % actionreq)            
+        ## print('data %s' % data)
+        data_string = json.dumps(data).encode(encoding)
+        ## print('data_string %s' % type(data_string))            
+        request = Request(actionreq,data_string)
+        ## print('Request %s' % request)         
+        response = urlopen(request)
+        ## print('response %s' % response)            
         rta=time.time()-start
         
-        assert response.code == 200
-        response_dict = json.loads(response.read())
-        
+        ### assert response.code == 200
+        result = json.loads(response.read())['result']
+        ## print('result %s' % result)
 
         # Check the contents of the response.
-        assert response_dict['success'] is True
-        result = response_dict['result']
+        ##assert response_dict['success'] is True
         if actionreq.endswith('group_show') :
             resplen=result['package_count']
         else:
             resplen=len(result)
 
-    except urllib2.URLError as e:
-        msg = "   [URLERROR] %s " % e
+    except (URLError,socket.timeout,IOError,ValueError,Exception) as e:
+        msg = "%s : %s - %s" % (e,actionreq,data_string)
         retcode = 2
-    except socket.timeout as e:
-        msg = "   [IOERROR] %s " % e
-        retcode = 2
-    except IOError as e:
-        msg = "   [IOERROR] %s " % e
-        retcode = 1
-    except ValueError as e:
-        msg = "   [IOERROR] %s " % e
-        retcode = 1    #catched
-    except Exception as e:
-        msg = "   [IOERROR] %s " % e
-        retcode = 3    #catched
     else:
         msg = '[OK]'
         retcode = 0
@@ -174,7 +172,7 @@ def checkProbes(args):
 
         else:
             if probe == 'ListDatasets' :
-                action='dataset_list'
+                action='package_list'
             elif probe == 'ListCommunities' :
                 action='group_list'
             elif probe == 'ShowGroupENES' :
