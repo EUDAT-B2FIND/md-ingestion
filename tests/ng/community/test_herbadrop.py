0fc02115f6be11f015833af0d14ab5a3515a7434
Lenzma <41636532+Lenzma@users.noreply.github.com>
2020-06-30 16:27:10 +0200

    Ng herbadrop (#47)
    * update herbadrop test with PID
    
    * updated herbadrop test and description in mapfile
    
    * updated map function to write only validated records

--- tests/ng/community/test_herbadrop.py
+++ tests/ng/community/test_herbadrop.py
@@ -51,3 +51,23 @@ def test_herbadrop_with_pid():
     doc = reader.read(jsonfile)
     assert 'http://hdl.handle.net/21.T12996/dc6e1c9e-06c6-11ea-99e1-525400276111' == doc.pid
     assert 'https://opendata.cines.fr/herbadrop-api/rest/data/mnhnftp/P03068284' == doc.metadata_access
+    assert 'Coronilla juncea L.' in doc.title[0]
+    # assert 'HERBARIUM MUSE' in doc.description[1]
+    assert ['Fabaceae'] == doc.tags
+    assert 'http://coldb.mnhn.fr/catalognumber/mnhn/p/p03068284' == doc.source
+    assert 'ark:/87895/1.90-2785993' == doc.related_identifier[0]
+    assert 'unavailable' == doc.creator[0]
+    assert 'MNHN' == doc.publisher[0]
+    assert 'CINES' == doc.contributor[0]
+    assert '2019' == doc.publication_year[0]
+    assert 'http://creativecommons.org/licenses/by/4.0/' == doc.rights[0]
+    assert 'MNHN' == doc.contact[0]
+    assert doc.open_access is True
+    assert 'und_UND' == doc.language[0]
+    assert 'StillImage|PRESERVED_SPECIMEN' == doc.resource_type[0]
+    assert 'image/jpeg' == doc.format[0]
+    assert 'Plant Sciences' == doc.discipline
+    assert 'HERBARIUM MUSEI PARISIENSIS' == doc.description[2]
+    # assert 'France|Languedoc-Roussillon||||Eyne' == doc.spatial_coverage
+    #assert '1968-07-17T00:00:00Z' == doc.temporal_coverage_begin_date
+    #assert '1968-07-17T00:00:00Z' == doc.temporal_coverage_end_date
