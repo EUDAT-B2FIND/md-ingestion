209df489eafdc06d1ce7f6de181660224dfc83cd
Lenzma <fluegel@dkrz.de>
2020-07-06 13:32:49 +0200

    added openaccess recognition to darus mapfile

--- mdingestion/ng/community/darus.py
+++ mdingestion/ng/community/darus.py
@@ -5,3 +5,9 @@ from ..format import format_value
 class DarusDatacite(DataCiteReader):
     def update(self, doc):
         doc.discipline = format_value(self.parser.find('subject'), type='string_words', one=True).split(' ')[0]
+        doc.open_access = self.open_access(doc)
+
+    def open_access(self, doc):
+        if self.parser.find('rights', rightsURI='info:eu-repo/semantics/openAccess'):
+            return True
+        return False
