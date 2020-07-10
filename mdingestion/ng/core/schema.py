9e5fb291d6836a8544e670bdc6ac05f4fb079915
seolih <martens@dkrz.de>
2020-07-10 16:39:11 +0200

    changed B2FIND schema a bit to EUDAT core

--- mdingestion/ng/core/schema.py
+++ mdingestion/ng/core/schema.py
@@ -2,11 +2,18 @@ import colander
 
 
 class B2FSchema(colander.MappingSchema):
+    community = colander.SchemaNode(
+        colander.String(),
+        name='community',
+        title='Community',
+        description='The scientific community, Research Infrastructure, Project or Data provider from which B2FIND harvests the metadata.',  # noqa
+        validator=colander.url,
+    )
     identifier = colander.SchemaNode(
         colander.String(),
         name='identifier',
         title='Identifier',
-        description='An identifier (URL) that uniquely identifies a resource. It may be a DOI, a PID or source URL.',  # noqa
+        description='A unique string that identifies a resource, ideally persistent. It may be a DOI, a PID or any other source.',  # noqa
         validator=colander.url,
     )
     title = colander.SchemaNode(
@@ -22,15 +29,16 @@ class B2FSchema(colander.MappingSchema):
         colander.SchemaNode(colander.String(), missing=colander.drop),
         name='description',
         title='Description',
-        description='An additional information describing the content of the resource. Could be an abstract, a summary or a Table of Content.',  # noqa
+        description='All additional information that does not fit in any of the other categories. May be used for technical information. It is a best practice to supply a description.',  # noqa
         missing=colander.drop,
     )
-    tags = colander.SchemaNode(
+    # TODO: decide how it is named in EUDAT core
+    keyword = colander.SchemaNode(
         colander.Sequence(accept_scalar=True),
         colander.SchemaNode(colander.String(), missing=colander.drop),
-        name='tags',
-        title='Tags',
-        description='A subject, keyword, classification code, or key phrase describing the content.',
+        name='keyword',
+        title='Keyword',
+        description='Subject, keyword, classification code, or key phrase describing the resource. Could be free text.',
         missing=colander.drop,
     )
     doi = colander.SchemaNode(
@@ -53,7 +61,7 @@ class B2FSchema(colander.MappingSchema):
         colander.String(),
         name='source',
         title='Source',
-        description='An identifier (URL) that uniquely identifies a resource. It may link to the data itself or a landing page that curates the data.',  # noqa
+        description='An identifier that uniquely identifies a resource. It may link to the data itself or a landing page that curates the data.',  # noqa
         missing=colander.drop,
         validator=colander.url,
     )
@@ -63,7 +71,7 @@ class B2FSchema(colander.MappingSchema):
         colander.SchemaNode(colander.String(), missing=colander.drop),
         name='related_identifier',
         title='RelatedIdentifier',
-        description='A link to related resources or supplements.',
+        description='Identifiers of related resources.',
         missing=colander.drop,
         # validator=colander.url,
     )
@@ -71,7 +79,7 @@ class B2FSchema(colander.MappingSchema):
         colander.String(),
         name='metadata_access',
         title='MetaDataAccess',
-        description='Is a link to the originally harvested metadata record (GetRecord request).',
+        description='Is a link to the originally harvested metadata record.',
         missing=colander.drop,
         validator=colander.url,
     )
@@ -80,7 +88,7 @@ class B2FSchema(colander.MappingSchema):
         colander.SchemaNode(colander.String(), missing=colander.drop),
         name='creator',
         title='Creator',
-        description='The main researchers involved in producing the data, or the authors of the publication, or the measurement or monitoring station that produces the data in priority order.',  # noqa
+        description='The main researchers involved working on the data, or the authors of the publication in priority order. May be a corporate/institutional or personal name.',  # noqa
         missing=colander.drop,
     )
     publisher = colander.SchemaNode(
@@ -99,12 +107,28 @@ class B2FSchema(colander.MappingSchema):
         description='The institution or person responsible for collecting, managing, distributing, or otherwise contributing to the development of the resource.',  # noqa
         missing=colander.drop,
     )
+    instrument = colander.SchemaNode(
+        colander.Sequence(accept_scalar=True),
+        colander.SchemaNode(colander.String(), missing=colander.drop),
+        name='instrument',
+        title='Instrument',
+        description='The technical instrument(s) used to generate (observe or measure) the data.',  # noqa
+        missing=colander.drop,
+    )
     publication_year = colander.SchemaNode(
         colander.Sequence(accept_scalar=True),
         colander.SchemaNode(colander.Date(), missing=colander.drop),
         name='publication_year',
         title='PublicationYear',
-        description='The institution or person responsible for collecting, managing, distributingThe year when the data was or will be made publicly available.',  # noqa
+        description='Year when the data is made publicly available. If an embargo period has been in effect, use the date when the embargo period ends.',  # noqa
+        missing=colander.drop,
+    )
+    funding_reference = colander.SchemaNode(
+        colander.Sequence(accept_scalar=True),
+        colander.SchemaNode(colander.String(), missing=colander.drop),
+        name='funding_reference',
+        title='FundingReference',
+        description='Information about financial support (funding) for the resource.',
         missing=colander.drop,
     )
     rights = colander.SchemaNode(
@@ -119,7 +143,7 @@ class B2FSchema(colander.MappingSchema):
         colander.Boolean(),
         name='open_access',
         title='OpenAccess',
-        description='Is the dataset openly accessible or not.',
+        description='Information on whether the resource is openly accessible or not.',
         missing=colander.drop,
     )
     contact = colander.SchemaNode(
@@ -127,15 +151,16 @@ class B2FSchema(colander.MappingSchema):
         colander.SchemaNode(colander.String(), missing=colander.drop),
         name='contact',
         title='Contact',
-        description='Any contact information for this resource.',
+        description='A reference to contact information.',
         missing=colander.drop,
     )
+    # TODO: we would like to use language mapping to ISO codes.
     language = colander.SchemaNode(
         colander.Sequence(accept_scalar=True),
         colander.SchemaNode(colander.String(), missing=colander.drop),
         name='language',
         title='Language',
-        description='The primary language of the resource.',
+        description='Language(s) of the resource.',
         missing=colander.drop,
     )
     resource_type = colander.SchemaNode(
@@ -143,7 +168,7 @@ class B2FSchema(colander.MappingSchema):
         colander.SchemaNode(colander.String(), missing=colander.drop),
         name='resource_type',
         title='ResourceType',
-        description='A description of the resource, a general type of the resource.',
+        description='The type(s) of the resource. Free text values allowed.',
         missing=colander.drop,
     )
     format = colander.SchemaNode(
@@ -151,25 +176,42 @@ class B2FSchema(colander.MappingSchema):
         colander.SchemaNode(colander.String(), missing=colander.drop),
         name='format',
         title='Format',
-        description='The mimetype of the resource.',
+        description='Technical format of the resource. Free text. Use file extension or MIME type where possible, e.g. PDF, XML, MPG or application/pdf, text/xml, video/mpeg.',
+        missing=colander.drop,
+    )
+    size = colander.SchemaNode(
+        colander.Sequence(accept_scalar=True),
+        colander.SchemaNode(colander.String(), missing=colander.drop),
+        name='size',
+        title='Size',
+        description='Size information about the resource.',
+        missing=colander.drop,
+    )
+    version = colander.SchemaNode(
+        colander.Sequence(accept_scalar=True),
+        colander.SchemaNode(colander.String(), missing=colander.drop),
+        name='version',
+        title='Version',
+        description='Version information about the resource.',
         missing=colander.drop,
     )
     discipline = colander.SchemaNode(
         colander.String(),
         name='discipline',
         title='Discipline',
-        description='A scientific discipline the resource originates from. A closed vocabulary is used.',
+        description='The research discipline(s) the resource can be categorized in.',
         missing=colander.drop,
     )
     spatial_coverage = colander.SchemaNode(
         colander.String(),
         name='spatial_coverage',
         title='SpatialCoverage',
-        description='A geolocation the research data itself is related to. Content of this category is displayed in plain text.  If a longitude/latitude information is given it will be displayed at the map.',  # noqa
+        description='The spatial coverage the research data itself is related to. Content of this category is displayed in plain text. If a longitude/latitude information is given it will be displayed on the map.',  # noqa
         missing=colander.drop,
     )
+    # NOTE: should enable both Date format and plain text like 'Viking age'
     temporal_coverage = colander.SchemaNode(
-        colander.DateTime(),
+        colander.String(),
         name='temporal_coverage',
         title='TemporalCoverage',
         description='Period of time the research data itself is related to. Could be a date format or plain text.',  # noqa
