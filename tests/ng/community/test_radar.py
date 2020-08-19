07105dd02fc46c3d5b4e47da4653088273b5b175
Lenzma <41636532+Lenzma@users.noreply.github.com>
2020-08-19 13:10:56 +0200

    Pangaeatest (#69)
    * updated test for radar_datacite
    
    * fixed/eliminated () after creators without affiliation in datacite.py

--- tests/ng/community/test_radar.py
+++ tests/ng/community/test_radar.py
@@ -1,15 +1,16 @@
 import os
 
-from mdingestion.ng.community.radar import RadarDublinCore
+from mdingestion.ng.community.radar import RadarDatacite
 
 from tests.common import TESTDATA_DIR
 
 
-def test_dublin_core():
-    xmlfile = os.path.join(TESTDATA_DIR, 'radar-oai_dc', 'SET_1', 'xml', '0747e419-cffb-5958-bb39-278df846de13.xml')  # noqa
-    reader = RadarDublinCore()
+def test_datacite():
+    xmlfile = os.path.join(TESTDATA_DIR, 'radar-datacite', 'SET_1', 'xml', '4b9e42ae-b56e-5485-9b45-7b00e859a98f.xml')  # noqa
+    reader = RadarDatacite()
     doc = reader.read(xmlfile)
-    assert 'Result files' in doc.title[0]
-    assert 'Various' in doc.discipline
-    assert 'CC BY 4.0 Attribution' in doc.rights
+    assert 'Raw data for' in doc.title[0]
+    assert 'Biochemistry' in doc.discipline
+    assert 'Deutsche Forschungsgemeinschaft' in doc.funding_reference
+    # assert 'CC BY 4.0 Attribution' in doc.rights
     # assert 'https://doi.org/10.22000/81' in doc.doi
