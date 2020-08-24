375223f29858051dcc6e20766d589a1f148da3c3
seolih <martens@dkrz.de>
2020-08-24 13:21:30 +0200

    new community specific mapfile for deims

--- mdingestion/ng/community/deims.py
+++ mdingestion/ng/community/deims.py
@@ -9,24 +9,24 @@ class DeimsISO19139(ISO19139Reader):
     def update(self, doc):
         doc.contributor = 'DEIMS-SDR Site and Dataset registry deims.org'
         doc.discipline = 'Environmental Monitoring'
-        doc.doi = self.find_doi('distributionInfo.URL')
-        doc.pid = self.find_pid('distributionInfo.URL')
-        doc.source = self.source(doc)
-        doc.related_identifier = self.related_identifier(doc)
+        #doc.doi = self.find_doi('distributionInfo.URL')
+        #doc.pid = self.find_pid('distributionInfo.URL')
+        #doc.source = self.source(doc)
+        #doc.related_identifier = self.related_identifier(doc)
 
-    def source(self, doc):
+    #def source(self, doc):
         # TODO: find with pattern
-        urls = [url for url in self.find('distributionInfo.URL') if 'deims.org/' in url]
-        return urls
+        #urls = [url for url in self.find('distributionInfo.URL') if 'deims.org/' in url]
+        #return urls
 
-    def related_identifier(self, doc):
-        urls = []
-        for url in self.find('distributionInfo.URL'):
-            if 'deims.org/' in url:
-                continue
-            if 'doi.org/' in url:
-                continue
-            if 'hdl.handle.net/' in url:
-                continue
-            urls.append(url)
-        return urls
+    #def related_identifier(self, doc):
+        #urls = []
+        #for url in self.find('distributionInfo.URL'):
+            #if 'deims.org/' in url:
+            #    continue
+            #if 'doi.org/' in url:
+            #    continue
+            #if 'hdl.handle.net/' in url:
+            #    continue
+            #urls.append(url)
+        #return urls
