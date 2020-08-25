9ce8d16aa5636b6f17caa1ba3c7cc22e3e628933
seolih <martens@dkrz.de>
2020-08-25 17:04:01 +0200

    new mapfiles for envidat and ucl, updated harvest_list_cm

--- mdingestion/ng/community/deims.py
+++ mdingestion/ng/community/deims.py
@@ -1,6 +1,9 @@
+from mdingestion.ng import classify
 from ..reader import ISO19139Reader
 from ..sniffer import CSWSniffer
 from ..format import format_value
+
+import logging
 
 
 class DeimsISO19139(ISO19139Reader):
@@ -14,6 +17,7 @@ class DeimsISO19139(ISO19139Reader):
         doc.discipline = 'Environmental Monitoring'
         doc.metadata_access = [url for url in self.find('linkage') if 'deims.org/api/' in url]
         self.related_identifier(doc)
+        #self.discipline(doc)
         self.fix_source(doc)
 
     def fix_source(self, doc):
@@ -37,3 +41,14 @@ class DeimsISO19139(ISO19139Reader):
                 continue
             urls.append(url)
         doc.related_identifier = urls
+
+    def discipline(self, doc):
+        classifier = classify.Classify()
+        result = classifier.map_discipline(doc.keywords)
+        if 'Various' not in result:
+            logging.debug(f"{result} keywords={doc.keywords}")
+        disc = result[0]
+        if 'Various' in disc:
+            disc = 'Environmental Monitoring'
+        doc.discipline = disc
+        return disc
--- mdingestion/ng/community/envidat.py
+++ mdingestion/ng/community/envidat.py
@@ -1,24 +1,14 @@
 from ..reader import (
     DataCiteReader,
-    ISO19139Reader,
 )
 from ..sniffer import (
     OAISniffer,
-    CSWSniffer,
 )
 
 
 class EnvidatDatacite(DataCiteReader):
-    NAME = 'envidat-oai_datacite'
+    NAME = 'envidat-datacite'
     SNIFFER = OAISniffer
-
-    def update(self, doc):
-        doc.contributor = 'EnviDat'
-
-
-class EnvidatISO19139(ISO19139Reader):
-    NAME = 'envidat-iso19139'
-    SNIFFER = CSWSniffer
 
     def update(self, doc):
         doc.contributor = 'EnviDat'
