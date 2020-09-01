8499b8da766f4a36e96ef8b62e87b33dfba470ca
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-09-01 15:51:26 +0200

    added danseasy test

---
+++ tests/ng/community/test_danseasy.py
@@ -0,0 +1,13 @@
+import os
+
+from mdingestion.ng.community.danseasy import DanseasyDatacite
+
+from tests.common import TESTDATA_DIR
+
+
+def test_datacite():
+    xmlfile = os.path.join(TESTDATA_DIR, 'danseasy-oai_datacite', 'SET_1', 'xml', '6ab25834-a565-5d61-b77d-f08f5b3c7b14.xml')  # noqa
+    reader = DanseasyDatacite()
+    doc = reader.read(xmlfile, url='', community='danseasy', mdprefix='oai_datacite')
+    assert ['info:eu-repo/semantics/closedAccess', 'DANS License'] == doc.rights
+    assert 'bla' == doc.open_access
