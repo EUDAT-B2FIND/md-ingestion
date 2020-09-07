fbdaee82512eb2737a9c8099d1d22456fd3b8e62
Lenzma <fluegel@dkrz.de>
2020-09-07 12:15:35 +0200

    updated darus mapfile with discipline function.

--- mdingestion/ng/community/darus.py
+++ mdingestion/ng/community/darus.py
@@ -1,8 +1,21 @@
+from mdingestion.ng import classify
 from ..reader import DataCiteReader
 from ..sniffer import OAISniffer
 from ..format import format_value
+
+import logging
 
 
 class DarusDatacite(DataCiteReader):
     NAME = 'darus-oai_datacite'
     SNIFFER = OAISniffer
+
+    def discipline(self, doc):
+        classifier = classify.Classify()
+        result = classifier.map_discipline(doc.keywords)
+        if 'Various' not in result:
+            logging.debug(f"{result} keywords={doc.keywords}")
+        disc = result[0]
+        # if 'Various' in disc:
+        #    disc = 'Earth System Research'
+        return disc
