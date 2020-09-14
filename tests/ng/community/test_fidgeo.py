e71c9d88f56799e03d6804711ed777d0cb6efc7b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-09-15 00:59:42 +0200

    fixed tests

--- tests/ng/community/test_fidgeo.py
+++ tests/ng/community/test_fidgeo.py
@@ -11,4 +11,4 @@ def test_fidgeo():
     doc = reader.read(xmlfile, url='', community='fidgeo', mdprefix='oai_datacite')
     assert ['CC BY 4.0'] == doc.rights
     assert doc.open_access is True
-    assert '; High Mountain Asia, 1987-2016' == doc.spatial_coverage
+    assert '(67.5W, 25.0S, 95.0E, 45.0N); High Mountain Asia, 1987-2016' == doc.spatial_coverage
