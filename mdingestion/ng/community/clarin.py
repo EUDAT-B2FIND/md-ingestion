8565543d6d22934b5af8dde31becf6cb56f9d9ec
seolih <martens@dkrz.de>
2020-09-21 11:32:18 +0200

    new mapfile for clarin

---
+++ mdingestion/ng/community/clarin.py
@@ -0,0 +1,19 @@
+from ..reader import DublinCoreReader
+from ..sniffer import OAISniffer
+
+
+class ClarinDublinCore(DublinCoreReader):
+    NAME = 'clarin-oai_dc'
+    SNIFFER = OAISniffer
+
+    def update(self, doc):
+        #doc.related_identifier = self.find('references')
+        doc.discipline = 'Linguistics'
+        doc.keywords = self.keywords(doc)
+        if not doc.publisher:
+            doc.publisher = 'CLARIN'
+
+    def keywords(self, doc):
+        keywords = doc.keywords
+        keywords.append('CLARIN')
+        return keywords
