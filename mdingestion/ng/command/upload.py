b3c31a0f14ab4c0f1ddeb6df40d05140578997ff
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-09-22 23:36:50 +0200

    using upload method

--- mdingestion/ng/command/upload.py
+++ mdingestion/ng/command/upload.py
@@ -9,6 +9,19 @@ from ..walker import Walker
 import logging
 
 
+def upload(data, host=None, apikey=None, verify=True):
+    requests_kwargs = None
+    if not verify:
+        requests_kwargs = {'verify': False}
+    with RemoteCKAN(f'http://{host}', apikey=apikey) as ckan:
+        try:
+            # ckan.action.package_update(**data)
+            ckan.call_action('package_update', data, requests_kwargs=requests_kwargs)
+        except NotFound:
+            # ckan.action.package_create(**data)
+            ckan.call_action('package_create', data, requests_kwargs=requests_kwargs)
+
+
 class Upload(Command):
     def run(self, iphost=None, auth=None, target=None, limit=None, verify=True):
         self.upload_to_ckan(iphost=iphost, auth=auth, limit=limit, verify=verify)
@@ -17,24 +30,15 @@ class Upload(Command):
         self.walker = Walker(self.outdir)
         limit = limit or -1
         count = 0
-        requests_kwargs = None
-        if not verify:
-            requests_kwargs = {'verify': False}
-        with RemoteCKAN(f'http://{iphost}', apikey=auth) as ckan:
-            for filename in tqdm(self.walk(), ascii=True, desc=f"Uploading {self.community}",
-                                 unit=' records', total=limit):
-                if limit > 0 and count >= limit:
-                    break
-                logging.info(f"uploading {filename}")
-                with open(filename, 'rb') as fp:
-                    data = json.load(fp)
-                    try:
-                        # ckan.action.package_update(**data)
-                        ckan.call_action('package_update', data, requests_kwargs=requests_kwargs)
-                    except NotFound:
-                        # ckan.action.package_create(**data)
-                        ckan.call_action('package_create', data, requests_kwargs=requests_kwargs)
-                count += 1
+        for filename in tqdm(self.walk(), ascii=True, desc=f"Uploading {self.community}",
+                             unit=' records', total=limit):
+            if limit > 0 and count >= limit:
+                break
+            logging.info(f"uploading {filename}")
+            with open(filename, 'rb') as fp:
+                data = json.load(fp)
+                upload(data=data, host=iphost, apikey=auth, verify=verify)
+            count += 1
 
     def walk(self):
         for filename in self.walker.walk_community(
