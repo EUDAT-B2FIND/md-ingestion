1b45689b5ceeae4fa78d50612c8230869b2269f0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-09-22 21:22:28 +0200

    update list command

--- mdingestion/ng/command/list.py
+++ mdingestion/ng/command/list.py
@@ -7,4 +7,4 @@ class List(Command):
 
     def run(self):
         for community in get_communities():
-            print(f"{community.NAME}, {community.IDENTIFIER}, {community.URL}, {community.SCHEMA}")
+            print(f"{community.NAME}, {community.IDENTIFIER}, {community.URL}, {community.SCHEMA}, {community.SERVICE_TYPE}")  # noqa
