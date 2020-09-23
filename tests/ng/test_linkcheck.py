52297bc59bdd9bea7fdf4e96dd8eb474d3ea852b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-09-23 16:51:33 +0200

    fix test linkcheck

--- tests/ng/test_linkcheck.py
+++ tests/ng/test_linkcheck.py
@@ -7,8 +7,8 @@ from tests.common import TESTDATA_DIR
 
 
 def test_linkcheck():
-    xml_file = os.path.join(TESTDATA_DIR, 'darus-oai_datacite', 'SET_1', 'xml', 'Discipline_30fec4d2-1f79-5192-9247-c67154475715.xml')  # noqa
+    xml_file = os.path.join(TESTDATA_DIR, 'darus', 'raw', 'Discipline_30fec4d2-1f79-5192-9247-c67154475715.xml')
     reader = DataCiteReader()
-    doc = reader.read(xml_file, url='https://darus.uni-stuttgart.de/oai', mdprefix='oai_datacite')
+    doc = reader.read(xml_file)
     lc = LinkChecker()
     lc.add(doc)
