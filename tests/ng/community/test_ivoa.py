8ad009b3895f3d89bbed355216bb0a778250f76d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-09-23 16:18:12 +0200

    fix test_ivoa

--- tests/ng/community/test_ivoa.py
+++ tests/ng/community/test_ivoa.py
@@ -1,14 +1,14 @@
 import os
 
-from mdingestion.ng.community.ivoa import IVOADatacite
+from mdingestion.ng.community.ivoa import IvoaDatacite
 
 from tests.common import TESTDATA_DIR
 
 
 def test_vo():
     xmlfile = os.path.join(TESTDATA_DIR, 'ivoa-oai_datacite', 'SET_1', 'xml', 'feeb0259-0155-5718-b5d1-3c37be822004.xml')  # noqa
-    reader = IVOADatacite()
-    doc = reader.read(xmlfile, url='http://dc.g-vo.org/rr/q/pmh/pubreg.xml', community='ivoa', mdprefix='oai_datacite')
+    reader = IvoaDatacite()
+    doc = reader.read(xmlfile)
     assert 'Kevin Benson <kmb(at)mssl.ucl.ac.uk>' in doc.contact
     assert 'http://cdaweb.gsfc.nasa.gov/cdaweb/sp_phys/' == doc.source
     assert ['International Virtual Observatory Alliance (IVOA)'] == doc.contributor
@@ -17,6 +17,6 @@ def test_vo():
 
 def test_vo_2():
     xmlfile = os.path.join(TESTDATA_DIR, 'ivoa-oai_datacite', 'SET_1', 'xml', '19cf53ec-4dc5-53d2-9da3-9b8b2ec2c1b7.xml')  # noqa
-    reader = IVOADatacite()
-    doc = reader.read(xmlfile, url='http://dc.g-vo.org/rr/q/pmh/pubreg.xml', community='ivoa', mdprefix='oai_datacite')
+    reader = IvoaDatacite()
+    doc = reader.read(xmlfile)
     assert 'https://ui.adsabs.harvard.edu/abs/2005AJ....130.2541M' in doc.related_identifier
