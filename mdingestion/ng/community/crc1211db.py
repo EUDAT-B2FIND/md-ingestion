a2510e1f6ebe94bd08cb5d008e3430fa4dd5b61f
seolih <martens@dkrz.de>
2020-09-23 10:54:30 +0200

    new mapfile for crc1211db

--- mdingestion/ng/community/crc1211db.py
+++ mdingestion/ng/community/crc1211db.py
@@ -1,7 +1,15 @@
-from ..reader import DataCiteReader
-from ..sniffer import OAISniffer
+from .base import Community
+from ..service_types import SchemaType, ServiceType
 
 
-class CRC1211DBDatacite(DataCiteReader):
-    NAME = 'crc1211db-oai_datacite'
-    SNIFFER = OAISniffer
+class CRC1211DBDatacite(Community):
+    NAME = 'crc1211db'
+    IDENTIFIER = 'crc1211db'
+    URL = 'http://doidb.wdc-terra.org/oaip/oai'
+    SCHEMA = SchemaType.DataCite
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_datacite'
+    OAI_SET = 'DOIDB.CRC1211'
+
+    def update(self, doc):
+        doc.contact = 'crc1211db-admin@uni-koeln.de'
