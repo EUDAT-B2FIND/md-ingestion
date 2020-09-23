c079151cb207503434db885bd71c3c80433c9933
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-09-23 16:42:33 +0200

    fix test dataverseno

--- tests/ng/community/test_dataverseno.py
+++ tests/ng/community/test_dataverseno.py
@@ -8,7 +8,7 @@ from tests.common import TESTDATA_DIR
 def test_dataverseno():
     xmlfile = os.path.join(TESTDATA_DIR, 'dataverseno-oai_dc', 'dataverseno', 'xml', '003d1010-19fb-5232-a762-7de457da5406.xml')  # noqa
     reader = DataverseNODublinCore()
-    doc = reader.read(xmlfile, url='', community='dataverseno', mdprefix='oai_dc')
+    doc = reader.read(xmlfile)
     assert 'https://doi.org/10.18710/DWAC63' == doc.doi
     assert 'Physics;History;History of Science' == doc.discipline
     assert ['DataverseNO'] == doc.publisher
