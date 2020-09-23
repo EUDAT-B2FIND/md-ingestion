d5eeb51c39e26fadfc8427d273be64af09319672
seolih <martens@dkrz.de>
2020-09-23 11:18:54 +0200

    new mapfile for danseasy

--- mdingestion/ng/community/danseasy.py
+++ mdingestion/ng/community/danseasy.py
@@ -1,15 +1,19 @@
-from ..reader import DataCiteReader
-from ..sniffer import OAISniffer
-from ..format import format_value
+from .base import Community
+from ..service_types import SchemaType, ServiceType
 
 
-class DanseasyDatacite(DataCiteReader):
-    NAME = 'danseasy-oai_datacite'
-    SNIFFER = OAISniffer
+class DansEasyDatacite(Community):
+    NAME = 'danseasy'
+    IDENTIFIER = 'danseasy'
+    URL = 'https://easy.dans.knaw.nl/oai'
+    SCHEMA = SchemaType.DataCite
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_datacite'
+    OAI_SET = None
 
     def update(self, doc):
         if not doc.doi:
             doc.doi = self.find_doi('alternateIdentifier')
         if not doc.source:
             doc.source = self.find_source('alternateIdentifier')
-        doc.discipline = self.discipline(doc)
+        #doc.discipline = self.discipline(doc)
