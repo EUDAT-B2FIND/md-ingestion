1a92586f08fe5b65c5f5b8d3aa557c877bcbbaef
seolih <martens@dkrz.de>
2020-10-02 20:52:18 +0200

    changes for pdc

--- mdingestion/ng/community/pdc.py
+++ mdingestion/ng/community/pdc.py
@@ -1,5 +1,6 @@
 from .base import Community
 from ..service_types import SchemaType, ServiceType
+from ..format import format_value, format_url
 
 
 class Pdc(Community):
@@ -16,3 +17,14 @@ class Pdc(Community):
             doc.contributor = 'Polar Data Catalogue'
         doc.discipline = self.discipline(doc, 'Environmental Research')
         doc.language = 'eng'
+        # TODO: extracting DOI from <publish>
+        # doc.doi = self.doi()
+
+    def doi(self):
+        url = ''
+        txt = format_value(self.find('metadata.publish'), one=True)
+        if 'doi' in txt:
+            print(txt)
+            value = txt.split('http://dx.doi.org/')[1]
+            url = format_url(f"http://dx.doi.org/{value}")
+        return url
