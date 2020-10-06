18ca1fc0d72bfc1eecc669690402bed9d606aae3
Lenzma <fluegel@dkrz.de>
2020-10-06 20:47:07 +0200

    created and/or updatd mapfles for enmap, geofon, gfz and gipp

--- mdingestion/ng/community/gipp.py
+++ mdingestion/ng/community/gipp.py
@@ -3,13 +3,14 @@ from ..service_types import SchemaType, ServiceType
 
 
 class GeofonDatacite(Community):
-    NAME = 'geofon'
+    NAME = 'gipp'
     IDENTIFIER = NAME
     URL = 'http://doidb.wdc-terra.org/oaip/oai'
     SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_datacite'
-    OAI_SET = 'DOIDB.SEISNET'
+    OAI_SET = 'DOIDB.GIPP'
 
     def update(self, doc):
         doc.discipline = self.discipline(doc, 'Seismology')
+        # TODO: discipline für GIPP dois 'Seismology, für Gipp-MT dois 'Geophysics'
