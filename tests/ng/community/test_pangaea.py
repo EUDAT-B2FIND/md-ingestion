1d8ef9c6afc387f912cb6e15cadda802112c7666
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-10-11 20:58:04 +0200

    added pangaea keywords test

--- tests/ng/community/test_pangaea.py
+++ tests/ng/community/test_pangaea.py
@@ -6,7 +6,7 @@ from tests.common import TESTDATA_DIR
 
 
 def test_pangaea():
-    xmlfile = os.path.join(TESTDATA_DIR, 'pangaea', 'raw', 'ef848f90-295c-54e6-b822-c0b66f8bec64.xml')  # noqa
+    xmlfile = os.path.join(TESTDATA_DIR, 'pangaea', 'raw', 'ef848f90-295c-54e6-b822-c0b66f8bec64.xml')
     reader = PangaeaDatacite()
     doc = reader.read(xmlfile)
     assert 'https://doi.pangaea.de/10.1594/PANGAEA.904099' == doc.doi
@@ -14,7 +14,7 @@ def test_pangaea():
 
 
 def test_pangaea_temporal():
-    xmlfile = os.path.join(TESTDATA_DIR, 'pangaea', 'raw', 'e4723633-ab46-5316-875f-fcd788dc0931.xml')  # noqa
+    xmlfile = os.path.join(TESTDATA_DIR, 'pangaea', 'raw', 'e4723633-ab46-5316-875f-fcd788dc0931.xml')
     reader = PangaeaDatacite()
     doc = reader.read(xmlfile)
     # assert 'https://doi.pangaea.de/10.1594/PANGAEA.904099' == doc.doi
@@ -28,17 +28,26 @@ def test_pangaea_temporal():
 
 
 def test_pangaea_bbox():
-    xmlfile = os.path.join(TESTDATA_DIR, 'pangaea', 'raw', 'c1d81bf1-758f-5b85-a93f-17827fce4900.xml')  # noqa
+    xmlfile = os.path.join(TESTDATA_DIR, 'pangaea', 'raw', 'c1d81bf1-758f-5b85-a93f-17827fce4900.xml')
     reader = PangaeaDatacite()
     doc = reader.read(xmlfile)
     assert 'https://doi.org/10.1594/PANGAEA.557786' == doc.doi
     assert '(0.000W, 78.000S, 7.500E, 80.000N); Fram Strait' == doc.spatial_coverage
 
+
 def test_pangaea_handle():
-    xmlfile = os.path.join(TESTDATA_DIR, 'pangaea', 'raw', '7c0209b8-6189-5bda-89eb-7d5231d6e72d.xml')  # noqa
+    xmlfile = os.path.join(TESTDATA_DIR, 'pangaea', 'raw', '7c0209b8-6189-5bda-89eb-7d5231d6e72d.xml')
     reader = PangaeaDatacite()
     doc = reader.read(xmlfile)
     assert 'https://doi.org/10.1594/PANGAEA.472241' == doc.doi
-    assert '(-33.649W, -74.241S, -16.500E, -71.869N); Camp Norway; Halley Bay; Weddell Sea; Lyddan Island' == doc.spatial_coverage
+    assert '(-33.649W, -74.241S, -16.500E, -71.869N); Camp Norway; Halley Bay; Weddell Sea; Lyddan Island' == doc.spatial_coverage  # noqa
     assert 'http://hdl.handle.net/10013/epic.14690.d001' == doc.pid
     # assert ['something(at)pangaea.de'] == doc.contact
+
+
+def test_pangaea_keywords():
+    xmlfile = os.path.join(TESTDATA_DIR, 'pangaea', 'raw', 'ee3796e4-4e01-5e4e-b2d8-ae67ab4b7381.xml')
+    reader = PangaeaDatacite()
+    doc = reader.read(xmlfile)
+    assert 'https://doi.org/10.1594/PANGAEA.889006' == doc.doi
+    assert ['EPD', 'DEPTH', 'sediment rock', 'Age', 'comment', 'dated standard deviation', 'Calendar age', 'maximum old', 'minimum young', '²¹⁰Pbₓₛ ¹³⁷Cs Lead Caesium', 'calibrated'] == doc.keywords  # noqa
