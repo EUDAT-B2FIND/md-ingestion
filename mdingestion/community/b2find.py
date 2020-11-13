3484ae5007fb45db0348ff876e7b90557c44f8b6
seolih <martens@dkrz.de>
2020-11-13 20:29:07 +0100

    new mapfile for harvesting B2FIND

---
+++ mdingestion/community/b2find.py
@@ -0,0 +1,15 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+
+
+class B2FINDDatacite(Community):
+    NAME = 'b2find'
+    IDENTIFIER = NAME
+    URL = 'http://eudat7-devel.dkrz.de/oai'
+    SCHEMA = SchemaType.DataCite
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_datacite'
+    OAI_SET = 'eudat-b2find'
+
+    def update(self, doc):
+        pass
