1ad896a6d2cf171d37ee380ada0c26366a5f4982
Carsten Ehbrecht <ehbrecht@dkrz.de>
2021-02-10 21:42:36 +0100

    update arcgis harvester

--- mdingestion/harvester/arcgis.py
+++ mdingestion/harvester/arcgis.py
@@ -22,6 +22,9 @@
 # Example for single object request:
 # https://kart.ra.no/arcgis/rest/services/Distribusjon/Kulturminner20180301/MapServer/7/query?where=+lokalid%3D%2722228%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&having=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentOnly=false&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&f=geojson
 #
+# arcgis docu:
+# https://kart.ra.no/arcgis/sdk/rest/index.html#//02ss0000000r000000
+#
 #
 # OR USE WFS
 # ----------
@@ -57,21 +60,41 @@ class ArcGISHarvester(Harvester):
     @property
     def query(self):
         if not self._query:
-            self._query = "where=+lokalid%3D%2722228%27&geometryType=esriGeometryEnvelope&spatialRel=esriSpatialRelIntersects&outFields=*&returnGeometry=true&returnTrueCurves=false&returnIdsOnly=false&returnCountOnly=false&returnZ=false&returnM=false&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&f=geojson"  # noqa
+            self._query = {
+                'where': 'lokalid>=0',
+                'outFields': '*',
+                'returnGeometry': True,
+                'f': 'geojson',
+                # 'returnIdsOnly': False,
+            }
         return self._query
 
     def identifier(self, record):
         return f"{record['id']}"
 
     def matches(self):
-        return 1
+        query = {
+            "returnCountOnly": True,
+        }
+        query.update(self.query)
+        response = requests.get(self.url, params=query, headers=self.headers, verify=self.verify)
+        return int(response.json()['count'])
 
     def get_records(self):
-        url = f"{self.url}?{self.query}"
-        response = requests.get(url, headers=self.headers, verify=self.verify)
-        records = response.json().get('features', [])
-        for record in records:
-            yield record
+        query = {
+            "resultOffset": 0,
+            "resultRecordCount": 200
+        }
+        query.update(self.query)
+        ok = True
+        while ok:
+            response = requests.get(self.url, params=query, headers=self.headers, verify=self.verify)
+            records = response.json().get('features', [])
+            for record in records:
+                yield record
+            if len(records) == 0:
+                ok = False
+            query['resultOffset'] += query['resultRecordCount']
 
     def _write_record(self, fp, record, pretty_print=True):
         json.dump(record, fp, indent=4, sort_keys=True, ensure_ascii=False)
