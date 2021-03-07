b0631c8d8a97d365b9f3f2688a77394c9fa67e13
Carsten Ehbrecht <ehbrecht@dkrz.de>
2021-03-07 20:23:52 +0100

    use pandas for list

--- mdingestion/command/list.py
+++ mdingestion/command/list.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 from .base import Command
 from ..community import community, communities
@@ -7,27 +8,26 @@ class List(Command):
 
     def run(self, name=None, groups=False, all=False, summary=False):
         name = name or 'all'
-        coms = {}
+        df = self.build_dataframe(name)
+        if all:
+            print(df)
+        elif groups:
+            print(df.Group.unique())
+        elif summary:
+            print(df.nunique())
+        else:
+            print(df.Community.unique())
+
+    def build_dataframe(self, name):
+        df = pd.DataFrame(columns=['Community', 'Group', 'Schema', 'Service', 'URL'])
+        pd.set_option('display.max_rows', None)
         for identifier in communities(name):
             com = community(identifier)
-            if com.NAME not in coms:
-                coms[com.NAME] = {}
-            coms[com.NAME][com.IDENTIFIER] = {
-                "url": com.URL, "schema": com.SCHEMA, "service_type": com.SERVICE_TYPE}
-        if all:
-            for name in coms:
-                for identifier in coms[name]:
-                    details = coms[name][identifier]
-                    print(f"{name}, {identifier}, {details['url']}, {details['schema']}, {details['service_type']}")
-        elif groups:
-            for name in coms:
-                for identifier in coms[name]:
-                    print(f"{identifier}")
-        elif summary:
-            groups = 0
-            for com in coms:
-                groups += len(coms[com])
-            print(f"Communities: {len(coms.keys())}, Groups: {groups}")
-        else:
-            for name in coms:
-                print(f"{name}")
+            df = df.append({
+                'Community': com.NAME,
+                'Group': com.IDENTIFIER,
+                'Schema': com.SCHEMA,
+                'Service': com.SERVICE_TYPE,
+                'URL': com.URL},
+                ignore_index=True)
+        return df
