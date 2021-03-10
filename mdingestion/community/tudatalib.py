e3a1909a9c1c4f4b3fe312e0bb1c0f59508a74c0
seolih <martens@dkrz.de>
2021-03-10 18:40:04 +0100

    fixed keywords in tudatalib

--- mdingestion/community/tudatalib.py
+++ mdingestion/community/tudatalib.py
@@ -2,7 +2,7 @@ from .base import Community
 from ..service_types import SchemaType, ServiceType
 
 
-class DarusDatacite(Community):
+class TudatalibDatacite(Community):
     NAME = 'tudatalib'
     IDENTIFIER = 'tudatalib'
     URL = 'https://tudatalib.ulb.tu-darmstadt.de/oai/openairedata'
@@ -13,3 +13,12 @@ class DarusDatacite(Community):
 
     def update(self, doc):
         doc.contact = 'https://tudatalib.ulb.tu-darmstadt.de/page/contact'
+        doc.keywords = self.keywords()
+
+    def keywords(self):
+        keywords = []
+        for subject in self.reader.parser.doc.find_all('subject'):
+            scheme = subject.get('subjectScheme')
+            if scheme is None or scheme == "DFG":
+                keywords.append(subject.text)
+        return keywords
