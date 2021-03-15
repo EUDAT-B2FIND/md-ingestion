f0d912eecba81441138130ae85f8f978a618a67e
Lenzma <fluegel@dkrz.de>
2021-03-15 19:22:34 +0100

    created keyword-discipline-mapping method for INRAE.

--- mdingestion/community/inrae.py
+++ mdingestion/community/inrae.py
@@ -1,6 +1,10 @@
 from .base import Community
 from ..service_types import SchemaType, ServiceType
 from ..format import format_value
+import pandas as pd
+
+
+CFG_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '..', 'etc', 'Community')
 
 
 class DataverseNODatacite(Community):
@@ -20,8 +24,15 @@ class DataverseNODatacite(Community):
             doc.pid = urls
         if not doc.publisher:
             doc.publisher = 'INRAE'
+        doc.discipline = self.discipline(doc, self.discipline_mapping(doc.keywords))
 
-    # def keywords(self, doc):
-        # keywords = doc.keywords
-        # keywords.append('EOSC Nordic')
-        # return keywords
+    def discipline_mapping(self, subjects):
+        fname = os.path.join(CFG_DIR, 'INRAE_MappingSubject__Discipline.csv')
+        df = pd.read_csv(open(fname))
+        values = []
+        for subject in subjects:
+            result = df.loc[df.Subject == subject]
+            result_discipline = list(result.Discipline.to_dict().values())[0]
+            values.extend(result_discipline.split(';'))
+        return values
+
