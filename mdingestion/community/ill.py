912d7d6a46be26093649147aa9dc6ecbb4c21f26
seolih <10978528+seolih@users.noreply.github.com>
2021-03-26 17:44:34 +0100

    new mapfiles for ill and esrf (#190)
    * new mapfiles for ill and esrf
    
    * fix spatium

---
+++ mdingestion/community/ill.py
@@ -0,0 +1,16 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+from ..format import format_value
+
+
+class ILLDatacite(Community):
+    NAME = 'ill'
+    IDENTIFIER = 'ill'
+    URL = 'https://data.ill.fr/openaire/oai'
+    SCHEMA = SchemaType.DataCite
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_datacite'
+    OAI_SET = 'openaire_data'
+
+    def update(self, doc):
+        doc.discipline = 'Particles, Nuclei and Fields'
