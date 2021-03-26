0c4823e8ca1d655814d290563aa2af0b46d4bc4f
seolih <martens@dkrz.de>
2021-03-26 16:10:49 +0100

    new mapfiles for ill and esrf

---
+++ mdingestion/community/esrf.py
@@ -0,0 +1,17 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+
+
+class ESRFDublinCore(Community):
+    NAME = 'esrf'
+    IDENTIFIER = 'esrf'
+    URL = 'https://icatplus.esrf.fr/oaipmh/request'
+    SCHEMA = SchemaType.DublinCore
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_dc'
+    OAI_SET = None
+
+    def update(self, doc):
+        doc.discipline = 'Particles, Nuclei and Fields'
+        doc.publication_year = '2020'
+        #doc.open_access = True
