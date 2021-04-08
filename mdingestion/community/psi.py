ccc96d2fe62d89449a87668c6dd6cf191b9bc54a
Lenzma <fluegel@dkrz.de>
2021-04-08 11:28:06 +0200

    just a test for psi oai-endpoint.

--- mdingestion/community/psi.py
+++ mdingestion/community/psi.py
@@ -5,12 +5,12 @@ from ..service_types import SchemaType, ServiceType
 class PsiDatacite(Community):
     NAME = 'psi'
     IDENTIFIER = NAME
-    URL = 'https://oai.datacite.org/oai'
+    URL = 'https://doi.psi.ch/'
     SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_datacite'
-    OAI_SET = 'ETHZ.PSI'
-    PRODUCTIVE = True
+    OAI_SET = None
+    PRODUCTIVE = False
 
     def update(self, doc):
         doc.discipline = self.discipline(doc, ['Life Sciences','Biology','Basic Biological and Medical Research'])
