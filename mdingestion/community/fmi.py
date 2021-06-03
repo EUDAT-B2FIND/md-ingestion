42ef3d0c27054cc6d2b459cfb49c9b7bdcfc1678
seolih <martens@dkrz.de>
2021-06-03 15:53:15 +0200

    new harvesting endpoint for fmi

--- mdingestion/community/fmi.py
+++ mdingestion/community/fmi.py
@@ -5,11 +5,11 @@ from ..service_types import SchemaType, ServiceType
 class FMI(Community):
     NAME = 'fmi'
     IDENTIFIER = NAME
-    URL = 'https://fmi.b2share.csc.fi/api/oai2d'
+    URL = 'https://hy.b2share.csc.fi/api/oai2d'
     SCHEMA = SchemaType.DublinCore
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_dc'
-    OAI_SET = '77f140b0-d4aa-437e-80d4-32c0abd3746f'
+    OAI_SET = '8343aaf9-f598-4529-9359-7ce2d9bd3e63'
 
     def update(self, doc):
         doc.discipline = self.discipline(doc, 'Meteorology')
