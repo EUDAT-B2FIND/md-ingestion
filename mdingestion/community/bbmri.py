7b3a135f415b70948597d3fc7f99cb33322e9f21
seolih <martens@dkrz.de>
2021-06-11 09:44:33 +0200

    new mapfile bbmri

---
+++ mdingestion/community/bbmri.py
@@ -0,0 +1,16 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+
+
+class Bbmri(Community):
+    NAME = 'bbmri'
+    IDENTIFIER = 'bbmri'
+    URL = 'https://b2share.eudat.eu/api/oai2d'
+    SCHEMA = SchemaType.DublinCore
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_dc'
+    OAI_SET = '99916f6f-9a2c-4feb-a342-6552ac7f1529'  # BBMRI
+    PRODUCTIVE = False
+
+    def update(self, doc):
+        doc.contributor = 'B2SHARE'
