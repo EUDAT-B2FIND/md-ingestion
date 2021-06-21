40541000f231caba89cb39c34b8c7c93d4f6c289
Carsten Ehbrecht <ehbrecht@dkrz.de>
2021-06-21 10:34:37 +0200

    fixed eudatcore harvest

--- mdingestion/community/eudatcsc.py
+++ mdingestion/community/eudatcsc.py
@@ -8,7 +8,7 @@ class EudatCSC(Community):
     URL = 'https://b2share.eudat.eu/api/oai2d'
     SCHEMA = SchemaType.Eudatcore
     SERVICE_TYPE = ServiceType.OAI
-    OAI_METADATA_PREFIX = 'oai-euc'
+    OAI_METADATA_PREFIX = 'eudatcore'
     OAI_SET = 'e9b9792e-79fb-4b07-b6b4-b9c2bd06d095'  # EUDAT
     PRODUCTIVE = False
 
