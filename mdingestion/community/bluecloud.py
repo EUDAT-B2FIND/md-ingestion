57526169fd4b33d1875d936274acf7a1516a3d90
seolih <martens@dkrz.de>
2021-06-29 15:15:52 +0200

    takeover changes from ALF bluecloud

--- mdingestion/community/bluecloud.py
+++ mdingestion/community/bluecloud.py
@@ -2,7 +2,7 @@ from .base import Community
 from ..service_types import SchemaType, ServiceType
 
 
-class Deims(Community):
+class Bluecloud(Community):
     NAME = 'bluecloud'
     IDENTIFIER = NAME
     URL = 'http://seadatanet.geodab.eu/gs-service/services/essi/view/seadatanet-open/csw'
@@ -10,12 +10,12 @@ class Deims(Community):
     SERVICE_TYPE = ServiceType.CSW
     PRODUCTIVE = False
 
-    # def update(self, doc):
-        # doc.source = self.find('transferOptions.linkage')
-        # doc.description = self.description()
+    def update(self, doc):
+        doc.source = self.find('transferOptions.linkage')
+        doc.description = self.description()
 
-    # def description(self):
-        # text = []
-        # text.extend(self.find('abstract'))
-        # text.extend(self.find('contentInfo.attributeDescription.RecordType'))
-        # return text
+    def description(self):
+        text = []
+        text.extend(self.find('abstract'))
+        text.extend(self.find('contentInfo.attributeDescription.RecordType'))
+        return text
