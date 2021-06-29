b2eed1da1b4574e21108c0485b8e8d1f104a992a
Lenzma <41636532+Lenzma@users.noreply.github.com>
2021-06-29 13:06:08 +0200

    Bluecloud (#202)
    * Created first draft mapfile for bluecloud.
    
    * updated bluecloud mapfile (prototype based on a seadatanet example - needs to be revised with actual bluecloud-xml).
    
    * fixed flake8 error.

---
+++ mdingestion/community/bluecloud.py
@@ -0,0 +1,21 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+
+
+class Bluecloud(Community):
+    NAME = 'bluecloud'
+    IDENTIFIER = NAME
+    URL = 'http://seadatanet.geodab.eu/gs-service/services/essi/view/seadatanet-open/csw'
+    SCHEMA = SchemaType.ISO19139
+    SERVICE_TYPE = ServiceType.CSW
+    PRODUCTIVE = False
+
+    def update(self, doc):
+        doc.source = self.find('transferOptions.linkage')
+        doc.description = self.description()
+
+    def description(self):
+        text = []
+        text.extend(self.find('abstract'))
+        text.extend(self.find('contentInfo.attributeDescription.RecordType'))
+        return text
