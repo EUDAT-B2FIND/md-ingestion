183b61ec54a5cce23df12d0b280bba02b18284f3
seolih <martens@dkrz.de>
2021-07-07 21:15:49 +0200

    new mapfile for datice

---
+++ mdingestion/community/datice.py
@@ -0,0 +1,17 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+
+
+class DaticeDatacite(Community):
+    NAME = 'datice'
+    IDENTIFIER = NAME
+    URL = 'https://oai.datacite.org/oai'
+    SCHEMA = SchemaType.DataCite
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_datacite'
+    OAI_SET = 'GESIS.SSRI'
+    PRODUCTIVE = False
+    Date = ''
+
+    def update(self, doc):
+        doc.discipline = 'Social Sciences'
