a231882b7cd69b60f295350fe51e5bcc32163bf8
seolih <martens@dkrz.de>
2022-02-03 16:02:40 +0100

    new mapfile for IVOA records in eudatcore

---
+++ mdingestion/community/ivoa_euc.py
@@ -0,0 +1,26 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+
+
+class IvoaEudatcore(Community):
+    NAME = 'ivoa_euc'
+    IDENTIFIER = 'ivoa_euc'
+    URL = 'http://dc.g-vo.org/rr/q/pmh/pubreg.xml'
+    SCHEMA = SchemaType.Eudatcore
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_b2find'
+    OAI_SET = None
+    PRODUCTIVE = False
+
+    def update(self, doc):
+        doc.source = self.find_source('relatedIdentifier', relatedIdentifierType="URL")
+        doc.related_identifier = self.find('relatedIdentifier', relatedIdentifierType="bibcode")
+        doc.contact = self.find('contributor', contributorType="ContactPerson")
+        doc.discipline = self.discipline(doc, 'Astrophysics and Astronomy')
+        doc.contributor = self.contributor(doc)
+        doc.contact = self.contact(doc)
+
+    def contributor(self, doc):
+        contributor = [name for name in doc.contributor if name not in doc.contact]
+        contributor.append('International Virtual Observatory Alliance (IVOA)')
+        return contributor
