c1e95f4935c08ee8fd041e7ecd02285998028e63
Lenzma <41636532+Lenzma@users.noreply.github.com>
2022-02-10 18:13:07 +0100

    Ceric (#226)
    * changed harvesting endpoint to productive instance.
    
    * fixed mapfile error.

--- mdingestion/community/ceric.py
+++ mdingestion/community/ceric.py
@@ -4,8 +4,8 @@ from ..service_types import SchemaType, ServiceType
 
 class CERICDatacite(Community):
     NAME = 'ceric'
-    IDENTIFIER = 'ceric'
-    URL = 'https://data.ceric-eric.eu/oai-pmh/oai2'
+    IDENTIFIER = NAME
+    URL = 'https://data.ceric-eric.eu/oaipmh/request'
     SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_datacite'
@@ -13,6 +13,4 @@ class CERICDatacite(Community):
 
     def update(self, doc):
         doc.discipline = 'Particles, Nuclei and Fields'
-        doc.open_access = True
-        doc.doi = ""
-        doc.source = self.find("metadata.identifier", identifierType='URL')
+        doc.source = self.find("datacite:identifier")
