6a388b331591a1542d7b9d4632dbb886281744a9
Lenzma <fluegel@dkrz.de>
2022-03-03 16:00:39 +0100

    created mapfile for european-xfel.

---
+++ mdingestion/community/european-xfel.py
@@ -0,0 +1,13 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+
+
+class EuropeanXFEL(Community):
+    NAME = 'european-xfel'
+    IDENTIFIER = NAME
+    URL = 'https://in.xfel.eu/metadata/oai-pmh/oai2'
+    SCHEMA = SchemaType.DataCite
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_datacite'
+    OAI_SET = None
+    PRODUCTIVE = True
