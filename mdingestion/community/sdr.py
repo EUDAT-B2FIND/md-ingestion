343aef4eff5b9a29ddc9e05ddec5bb0445206673
Lenzma <fluegel@dkrz.de>
2022-03-21 07:24:19 +0100

    updated mapfile with default publisher 'SURF Data Repository' (if none given).

--- mdingestion/community/sdr.py
+++ mdingestion/community/sdr.py
@@ -2,7 +2,7 @@ from .base import Community
 from ..service_types import SchemaType, ServiceType
 
 
-class SDRBase(Community):
+class SDRDublinCore(Community):
     NAME = 'sdr'
     IDENTIFIER = NAME
     URL = 'https://repository.surfsara.nl/api/oai2'
@@ -10,3 +10,9 @@ class SDRBase(Community):
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_dc'
     OAI_SET = None
+#   PRODUCTIVE = True
+#   DATE = '2022-03-20'
+
+    def update(self, doc):
+        if not doc.publisher:
+            doc.publisher = 'SURF Data Repository'
