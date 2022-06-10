42249ee3bc0a76d0030a22550a84f58d4236a6a0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-06-10 16:08:14 +0200

    added contert_to_lon_180

--- mdingestion/util.py
+++ mdingestion/util.py
@@ -17,3 +17,16 @@ def is_valid_email(value):
     if colander.Email().match_object.match(value) is None:
         return False
     return True
+
+def convert_to_lon_180(lon):
+    # converts longitude value to range [-180, 180]
+    #
+    # Longitude can be between 0~360 and -180~180.
+    #
+    # see:
+    # https://confluence.ecmwf.int/pages/viewpage.action?pageId=149337515
+    if lon > 180 or lon < 0:
+        lon_180 = (lon + 180) % 360 - 180
+    else:
+        lon_180 = lon
+    return lon_180 
--- tests/test_util.py
+++ tests/test_util.py
@@ -10,3 +10,12 @@ def test_remove_duplicates_from_list():
 def test_is_valid_url():
     assert util.is_valid_url('http://localhost:5000/csw') is True
     assert util.is_valid_url('http://localhost:5000/csw &bla') is False
+
+def test_convert_to_lon_180():
+    assert util.convert_to_lon_180(0) == 0
+    assert util.convert_to_lon_180(180) == 180
+    assert util.convert_to_lon_180(-180) == -180
+    assert util.convert_to_lon_180(360) == 0
+    assert util.convert_to_lon_180(190) == -170
+    assert util.convert_to_lon_180(-10) == -10
+    assert util.convert_to_lon_180(-190) == 170
