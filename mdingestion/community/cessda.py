790052b54aedfee7ce905acf6bdf1ced715da4a6
seolih <10978528+seolih@users.noreply.github.com>
2022-07-04 15:59:40 +0200

    new harvesting endpoint for open machine (no dev, no login) (#248)

--- mdingestion/community/cessda.py
+++ mdingestion/community/cessda.py
@@ -5,7 +5,7 @@ from ..service_types import SchemaType, ServiceType
 class CessdaDDI25(Community):
     NAME = 'cessda'
     IDENTIFIER = NAME
-    URL = 'https://datacatalogue-dev.cessda.eu/oai-pmh/v0/oai'
+    URL = 'https://datacatalogue.cessda.eu/oai-pmh/v0/oai'
     SCHEMA = SchemaType.DDI25
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_ddi25'
