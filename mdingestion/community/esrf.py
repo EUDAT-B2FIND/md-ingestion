941c89c2a6725401e4f1c3f162212e8b6c255a3f
Lenzma <fluegel@dkrz.de>
2022-07-12 18:46:40 +0200

    changed md-harvesting schema to oai_datacite (has DOIs).

--- mdingestion/community/esrf.py
+++ mdingestion/community/esrf.py
@@ -2,22 +2,23 @@ from .base import Community
 from ..service_types import SchemaType, ServiceType
 
 
-class ESRFDublinCore(Community):
+class ESRFDatacite(Community):
     NAME = 'esrf'
     IDENTIFIER = 'esrf'
     URL = 'https://icatplus.esrf.fr/oaipmh/request'
-    SCHEMA = SchemaType.DublinCore
+    SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
-    OAI_METADATA_PREFIX = 'oai_dc'
+    OAI_METADATA_PREFIX = 'oai_datacite'
     OAI_SET = None
 
     def update(self, doc):
         doc.discipline = 'Particles, Nuclei and Fields'
-        doc.publication_year = '2020'
-        doc.keywords = self.keywords(doc)
-        # doc.open_access = True
+        doc.publication_year =
+        doc.temporal_coverage =
+#        doc.keywords = self.keywords(doc)
+        doc.publisher = 'ESRF (European Synchrotron Radiation Facility)'
 
-    def keywords(self, doc):
+#    def keywords(self, doc):
         keywords = doc.keywords
         keywords.append('PaN')
         return keywords
