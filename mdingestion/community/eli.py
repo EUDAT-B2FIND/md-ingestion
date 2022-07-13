116e55498d4b9a7fe34e8730340ff1b1aea91dd5
seolih <martens@dkrz.de>
2022-07-13 10:31:30 +0200

    new mapfile for ELI ERIC testingestion

---
+++ mdingestion/community/eli.py
@@ -0,0 +1,14 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+
+
+class EliDataCite(Community):
+    NAME = 'eli'
+    IDENTIFIER = NAME
+    URL = 'https://data.eli-laser.eu/oai2d'
+    SCHEMA = SchemaType.DataCite
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_datacite'
+    OAI_SET = None
+#   PRODUCTIVE = True
+#   DATE = '2021-10-20'
