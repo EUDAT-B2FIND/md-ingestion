21e2f61d4b5c298ec7cf9fa13da5552c460ee49c
Lenzma <fluegel@dkrz.de>
2022-07-13 12:49:41 +0200

    cleaned up mapfile.

--- mdingestion/community/panosc.py
+++ mdingestion/community/panosc.py
@@ -30,13 +30,13 @@ class ESRFDatacite(BasePanosc):
         doc.discipline = 'Particles, Nuclei and Fields'
         doc.publication_year = self.publicationyear(doc)
 #        doc.temporal_coverage = <dates><date dateType="Collected">2022-04-26T07:30:00Z/2022-04-27T06:00:00Z</date>
-#        doc.keywords = self.keywords(doc)
+        doc.keywords = self.keywords(doc)
         doc.publisher = 'ESRF (European Synchrotron Radiation Facility)'
 
-#    def keywords(self, doc):
-#        keywords = doc.keywords
-#        keywords.append('PaN')#
-#        return keywords
+    def keywords(self, doc):
+        keywords = doc.keywords
+        keywords.append('PaN')
+        return keywords
 
     def publicationyear (self, doc):
         pubyear = doc.publication_year
