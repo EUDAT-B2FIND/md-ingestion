78e7103991c40d7a4b19c4aa41cca46df8227a5e
Lenzma <fluegel@dkrz.de>
2022-07-14 16:05:03 +0200

    updated ess harvesting endpoint. started configuring ILL mapfile.

--- mdingestion/community/ess.py
+++ mdingestion/community/ess.py
@@ -6,13 +6,14 @@ class EssDatacite(BasePanosc):
     NAME = 'ess'
     TITLE = 'ESS'
     IDENTIFIER = NAME
-    URL = 'https://scicat.esss.se/openaire/oai'
+#    URL = 'https://scicat.esss.se/openaire/oai'
+    URL = 'https://oai.panosc.ess.eu/openaire/oai'
     SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_datacite'
     OAI_SET = None
     PRODUCTIVE = True
-    DATE = '2021-03-14'
+    DATE = '2020-11-13'
     DESCRIPTION = "The European Spallation Source ESS is a European Research Infrastructure Consortium (ERIC), a multi-disciplinary research facility based on the world’s most powerful neutron source. Our vision is to build and operate the world’s most powerful neutron source, enabling scientific breakthroughs in research related to materials, energy, health and the environment, and addressing some of the most important societal challenges of our time. SciCat is the Metadata Catalogue at European Spallation Source."
     LOGO = ''
     # harvesting with: b2f harvest -c ess -k
