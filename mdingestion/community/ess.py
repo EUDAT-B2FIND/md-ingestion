345b80e28897c7543ebacf25e73b6617e8299ddc
Lenzma <fluegel@dkrz.de>
2022-07-15 09:34:55 +0200

    changed harvesting endpoint.

--- mdingestion/community/ess.py
+++ mdingestion/community/ess.py
@@ -6,7 +6,7 @@ class EssDatacite(BasePanosc):
     NAME = 'ess'
     TITLE = 'ESS'
     IDENTIFIER = NAME
-#    URL = 'https://scicat.esss.se/openaire/oai'
+#    URL = 'https://scicat.esss.se/openaire/oai' ##old endpoint
     URL = 'https://oai.panosc.ess.eu/openaire/oai'
     SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
@@ -16,7 +16,6 @@ class EssDatacite(BasePanosc):
     DATE = '2020-11-13'
     DESCRIPTION = "The European Spallation Source ESS is a European Research Infrastructure Consortium (ERIC), a multi-disciplinary research facility based on the world’s most powerful neutron source. Our vision is to build and operate the world’s most powerful neutron source, enabling scientific breakthroughs in research related to materials, energy, health and the environment, and addressing some of the most important societal challenges of our time. SciCat is the Metadata Catalogue at European Spallation Source."
     LOGO = ''
-    # harvesting with: b2f harvest -c ess -k
 
     def update(self, doc):
         doc.doi = self.find_doi('identifier', identifierType="URL")
