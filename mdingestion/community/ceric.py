df6746d40a65affc650d61e11fa4ec44896a1c30
Lenzma <fluegel@dkrz.de>
2022-07-18 10:07:36 +0200

    fixed mapping for ceric.

--- mdingestion/community/ceric.py
+++ mdingestion/community/ceric.py
@@ -1,9 +1,10 @@
-from .base import Community
+from .panosc import BasePanosc
 from ..service_types import SchemaType, ServiceType
 
 
-class CERICDatacite(Community):
+class CERICDatacite(BasePanosc):
     NAME = 'ceric'
+    TITLE = 'CERIC'
     IDENTIFIER = NAME
     URL = 'https://data.ceric-eric.eu/oaipmh/request'
     SCHEMA = SchemaType.DataCite
@@ -11,6 +12,36 @@ class CERICDatacite(Community):
     OAI_METADATA_PREFIX = 'oai_datacite'
     OAI_SET = None
 
+    PRODUCTIVE = True
+    DATE = '2022-07-18'
+    DESCRIPTION = "CERIC is a European Research Infrastructure Consortium (ERIC) integrating and providing open access to some of the best facilities in Europe, to help science and industry advance in all fields of materials, biomaterials and nanotechnology. With a single entry point to some of the leading national research infrastructures in 8 European countries, it enables the delivery of innovative solutions to societal challenges in the fields of energy, health, food, cultural heritage and more."
+    LOGO = ''
+
     def update(self, doc):
         doc.discipline = 'Particles, Nuclei and Fields'
-        doc.source = self.find("datacite:identifier")
+        doc.keywords = self.keywords(doc)
+        doc.temporal_coverage = self.find('dates.date', dateType="Collected")
+        doc.publication_year = self.publication_year(doc)
+        doc.title =self.title(doc)
+
+    def keywords(self, doc):
+        keywords = doc.keywords
+        keywords.append('PaN')
+        return keywords
+
+    def publication_year(self,doc):
+        pubyear = doc.publication_year
+        if not pubyear:
+            pubyear = self.find('dates.date', dateType="Accepted")
+        if not pubyear:
+            pubyear = self.find('header.datestamp')
+        return pubyear
+
+    def title(self,doc):
+        title = self.find('titles.title')
+        if not title:
+            title = 'Untitled'
+        elif len(title[0]) < 4:
+            title = 'Untitled'
+        return title
+
