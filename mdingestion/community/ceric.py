03e18c723d42e20de43725dda768d145a1fc16e2
Lenzma <fluegel@dkrz.de>
2022-07-19 11:44:05 +0200

    updated mapfile.

--- mdingestion/community/ceric.py
+++ mdingestion/community/ceric.py
@@ -13,7 +13,7 @@ class CERICDatacite(BasePanosc):
     OAI_SET = None
 
     PRODUCTIVE = True
-    DATE = '2022-07-18'
+    DATE = '2022-07-19'
     DESCRIPTION = "CERIC is a European Research Infrastructure Consortium (ERIC) integrating and providing open access to some of the best facilities in Europe, to help science and industry advance in all fields of materials, biomaterials and nanotechnology. With a single entry point to some of the leading national research infrastructures in 8 European countries, it enables the delivery of innovative solutions to societal challenges in the fields of energy, health, food, cultural heritage and more."
     LOGO = ''
 
@@ -21,18 +21,17 @@ class CERICDatacite(BasePanosc):
         doc.discipline = 'Particles, Nuclei and Fields'
         doc.keywords = self.keywords(doc)
         doc.temporal_coverage = self.find('dates.date', dateType="Collected")
-        doc.title =self.title(doc)
 
     def keywords(self, doc):
         keywords = doc.keywords
         keywords.append('PaN')
         return keywords
 
-    def title(self,doc):
-        title = self.find('titles.title')
-        if not title:
-            title = 'Untitled'
-        elif len(title[0]) < 4:
-            title = 'Untitled'
-        return title
+   # def title(self,doc):
+   #     title = self.find('titles.title')
+   #     if not title:
+   #         title = 'Untitled'
+   #     elif len(title[0]) < 4:
+   #         title = 'Untitled'
+   #     return title
 
