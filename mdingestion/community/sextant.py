5d41a105fb71a4d918bf995248728986712f10dd
seolih <martens@dkrz.de>
2022-09-20 13:00:10 +0200

    new mapfiles for b2share as community and sextant as repository

---
+++ mdingestion/community/sextant.py
@@ -0,0 +1,33 @@
+from .base import Community
+from ..service_types import SchemaType, ServiceType
+from ..format import format_value
+
+
+class Sextant(Community):
+    NAME = 'sextant'
+    IDENTIFIER = NAME
+    URL = 'https://sextant.ifremer.fr/geonetwork/srv/fre/csw'
+    SCHEMA = SchemaType.ISO19139
+    SERVICE_TYPE = ServiceType.CSW
+    PRODUCTIVE = False
+
+#    def update(self, doc):
+#        doc.doi = self.find_doi('linkage')
+#        doc.pid = self.find_pid('linkage')
+#        doc.source = self.find('MD_Identifier')
+#        doc.contributor = 'DEIMS-SDR Site and Dataset registry deims.org'
+#        doc.discipline = 'Environmental Monitoring'
+#        if not doc.publisher:
+#            doc.publisher = 'DEIMS-SDR'
+#        # TODO: why do we not use csw-metadataccess? It points to dc metadata though...fix it, Carsten!
+#        doc.metadata_access = [url for url in self.find('linkage') if 'deims.org/api/' in url]
+#        doc.discipline = self.discipline(doc, 'Environmental Monitoring')
+#        self.fix_source(doc)
+
+#    def fix_source(self, doc):
+#        if not doc.source:
+#            source = format_value(self.find('MD_Identifier'), one=True)
+#            if 'xlink' in source:
+#                doc.source = source.split()[1]
+#        if doc.source.startswith("https://deims.org/datasets/"):
+#            doc.source = doc.source.replace("https://deims.org/datasets/", "https://deims.org/dataset/")
