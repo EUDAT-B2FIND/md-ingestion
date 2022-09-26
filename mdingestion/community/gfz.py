7320ebea6da2af34a3a25eabe87400b47a83310f
Lenzma <41636532+Lenzma@users.noreply.github.com>
2022-09-27 00:02:33 +0200

    Gfz shift (#260)
    * editing gfz mapfile to shift communities and repos in ckan.
    
    * finished gfz group-mapfile.

--- mdingestion/community/gfz.py
+++ mdingestion/community/gfz.py
@@ -1,66 +1,84 @@
+from shapely.geometry import shape
+import json
+import pandas as pd
+import os
+import copy
+
 from .base import Community
 from ..service_types import SchemaType, ServiceType
 from ..format import format_value
 
 
 class BaseGfz(Community):
-    NAME = 'gfz'
+    GROUP = 'gfz'
+    GROUP_TITLE = 'GFZ Data Services'
+    PRODUCTIVE = True
+    DATE = '2021-08-15'
+    DESCRIPTION = 'By the GFZ Data Services the GFZ archives and publishes datasets to which a DOI has been assigned and which cover all geoscientific disciplines.'
+    LOGO = ''
     URL = 'http://doidb.wdc-terra.org/oaip/oai'
     SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_datacite'
-#    OAI_SET = 'DOIDB.GFZ'
-    PRODUCTIVE = False
+
 
     def update(self, doc):
         doc.discipline = self.discipline(doc, 'Geosciences')
 
 
 class Gfzdb(BaseGfz):
-    GROUP = 'gfzdataservices'
-    IDENTIFIER = GROUP
+    NAME = 'gfzdataservices'
+    TITLE = 'GFZ Data Services'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.GFZ'
 
 
 class GfzCrc1211db(BaseGfz):
-    GROUP = 'crc1211db'
-    IDENTIFIER = GROUP
+    NAME = 'crc1211db'
+    TITLE = 'CRC 1211 Database'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.CRC1211'
 
 
 class GfzEnmap(BaseGfz):
-    GROUP = 'enmap'
-    IDENTIFIER = GROUP
+    NAME = 'enmap'
+    TITLE = 'EnMAP'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.ENMAP'
 
 
 class GfzFidgeo(BaseGfz):
-    GROUP = 'fidgeo'
-    IDENTIFIER = GROUP
+    NAME = 'fidgeo'
+    TITLE = 'FID GEO'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.FID'
 
 
 class GfzGeofon(BaseGfz):
-    GROUP = 'geofon'
-    IDENTIFIER = GROUP
+    NAME = 'geofon'
+    TITLE = 'GEOFON Seismic Networks'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.GEOFON'
 
 
 class GfzGipp(BaseGfz):
-    GROUP = 'gipp'
-    IDENTIFIER = GROUP
+    NAME = 'gipp'
+    Title = 'GIPP - Geophysical Instrument Pool Potsdam'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.GIPP'
 
 
 class GfzIcgem(BaseGfz):
-    GROUP = 'icgem'
-    IDENTIFIER = GROUP
+    NAME = 'icgem'
+    TITLE = 'ICGEM - International Centre for Global Earth Models'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.ICGEM'
 
 
 class GfzIgets(BaseGfz):
-    GROUP = 'igets'
-    IDENTIFIER = GROUP
+    NAME = 'igets'
+    TITLE = 'IGETS - International Geodynamics and Earth Tide Service'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.IGETS'
 
     def update(self, doc):
@@ -68,36 +86,41 @@ class GfzIgets(BaseGfz):
 
 
 class GfzIntermagnet(BaseGfz):
-    GROUP = 'intermagnet'
-    IDENTIFIER = GROUP
+    NAME = 'intermagnet'
+    TITLE = 'INTERMAGNET'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.INTERMAG'
 
 
 # class GfzIsdc(BaseGfz):
-#    GROUP = 'isdc'
-#    IDENTIFIER = GROUP
+#    NAME = 'isdc'
+#    IDENTIFIER = NAME
 #    OAI_SET = ' DOIDB.ISDC'
 
 
 class GfzIsg(BaseGfz):
-    GROUP = 'isg'
-    IDENTIFIER = GROUP
+    NAME = 'isg'
+    TITLE = 'ISG - International Service for the Geoid'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.ISG'
 
 
 class GfzPik(BaseGfz):
-    GROUP = 'pik'
-    IDENTIFIER = GROUP
+    NAME = 'pik'
+    TITLE = 'PIK - Potsdam Institute for Climate Impact Research'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.PIK'
 
 
 class GfzTereno(BaseGfz):
-    GROUP = 'tereno'
-    IDENTIFIER = GROUP
+    NAME = 'tereno'
+    TITLE = 'TERENO'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.TERENO'
 
 
 class GfzWsm(BaseGfz):
-    GROUP = 'wsm'
-    IDENTIFIER = GROUP
+    NAME = 'wsm'
+    TITLE = 'WSM - World Stress Map'
+    IDENTIFIER = NAME
     OAI_SET = 'DOIDB.WSM'
