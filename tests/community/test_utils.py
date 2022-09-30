1f50074b7dc71c15689fc335674f61fb71faabf5
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-09-30 20:24:48 +0200

    update community test

--- tests/community/test_utils.py
+++ tests/community/test_utils.py
@@ -1,4 +1,11 @@
 from mdingestion.community import communities, community
+from mdingestion.community import get_communities
 
 def test_communities():
-    pass
+    assert communities("darus") == ['darus']
+    assert communities("pangaea") == ['pangaea']
+    assert communities("fmi") == ['fmi']
+    # assert communities("b2share") == ['b2share']
+
+def test_get_communities():
+    assert len(get_communities()) > 100
