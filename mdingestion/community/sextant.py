4ad28692db0852abae248be9d55d0e74f12c13c3
Lenzma <fluegel@dkrz.de>
2022-11-14 12:01:16 +0100

    fixed typo.

--- mdingestion/community/sextant.py
+++ mdingestion/community/sextant.py
@@ -30,21 +30,8 @@ class Sextant(Community):
 
     def publisher(self, doc):
         if not doc.publisher:
-            doc.publisher = 'Ifremer'
+            doc.publisher = 'IFREMER'
 
     def discipline(self, doc):
         if not doc.discipline:
             doc.discipline = 'Oceanography/Marine Science'
-
-#        # TODO: why do we not use csw-metadataccess? It points to dc metadata though...fix it, Carsten!
-#        doc.metadata_access = [url for url in self.find('linkage') if 'deims.org/api/' in url]
-#        doc.discipline = self.discipline(doc, 'Environmental Monitoring')
-#        self.fix_source(doc)
-
-#    def fix_source(self, doc):
-#        if not doc.source:
-#            source = format_value(self.find('MD_Identifier'), one=True)
-#            if 'xlink' in source:
-#                doc.source = source.split()[1]
-#        if doc.source.startswith("https://deims.org/datasets/"):
-#            doc.source = doc.source.replace("https://deims.org/datasets/", "https://deims.org/dataset/")
