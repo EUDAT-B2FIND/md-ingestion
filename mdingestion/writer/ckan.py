81d862e45ebcd9360b5ef3250b24854e4ee3b681
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-12-20 15:16:46 +0100

    clean up

--- mdingestion/writer/ckan.py
+++ mdingestion/writer/ckan.py
@@ -13,10 +13,8 @@ def map_ckan_fields(fields):
     for key, value in fields.items():
         if value:
             if key == 'author':
-                print(value)
                 if isinstance(value, list):
                     value = '; '.join(value)
-                print(value)
             elif key in ['title', 'notes']:
                 if isinstance(value, list):
                     value = '\n\n'.join(value)
