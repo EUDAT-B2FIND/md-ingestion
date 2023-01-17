d90ead1dced3331210b69c2320b50a3020479d8d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2023-01-17 14:02:23 +0100

    update cron command

--- mdingestion/command/crongen.py
+++ mdingestion/command/crongen.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import datetime as dt
 
 from .base import Command
 from ..community import repo, repos
@@ -15,11 +15,20 @@ class CronGen(Command):
     def run(self, name=None, out=None):
         name = name or 'all'
 
+        t = dt.datetime(2023, 1, 1, 0, 0)
+        delta = dt.timedelta(minutes=10)
+
         repo_list = []
         for identifier in repos(name):
             a_repo = repo(identifier)
             if a_repo.PRODUCTIVE:
-                repo_list.append(a_repo)
+                t = t + delta
+                cron_info = {
+                    'identifier': a_repo.identifier,
+                    'hour': t.hour,
+                    'minute': t.minute,
+                }
+                repo_list.append(cron_info)
         b2f_cron = env.get_template("b2f_cron")
         b2f_cron.render(repos=repo_list)
         with open(out, mode="w", encoding="utf-8") as fp:
