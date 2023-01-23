546566e47ce7f55f59a949a0c7649186e3f6b270
seolih <martens@dkrz.de>
2023-01-23 15:00:17 +0100

    updates in repo mapfile for eudat and comm mapfile for b2share (adding GROUP='b2share')

--- mdingestion/community/eudat.py
+++ mdingestion/community/eudat.py
@@ -5,12 +5,17 @@ from ..service_types import SchemaType, ServiceType
 class BaseEudat(Repository):
     IDENTIFIER = 'eudat'
     NAME = 'eudat'
-    PRODUCTIVE = False
-    DATE = ''
+    PRODUCTIVE = True
+    DATE = '2023-01-23'
+
+    def update(self, doc):
+        if not doc.publication_year:
+            doc.publication_year = self.find('header.datestamp')
 
 
 class EudatCsc(BaseEudat):
     IDENTIFIER = 'eudat_csc'
+    GROUP = 'b2share'
     URL = 'https://b2share.eudat.eu/api/oai2d'
     SCHEMA = SchemaType.Eudatcore
     SERVICE_TYPE = ServiceType.OAI
@@ -20,6 +25,7 @@ class EudatCsc(BaseEudat):
 
 class EudatFzj(BaseEudat):
     IDENTIFIER = 'eudat_fzj'
+    GROUP = 'b2share'
     URL = 'https://b2share.fz-juelich.de/api/oai2d'
     SCHEMA = SchemaType.Eudatcore
     SERVICE_TYPE = ServiceType.OAI
