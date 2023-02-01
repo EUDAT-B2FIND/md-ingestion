56a8d8890b28c57f87fd4a111103034375f54eb9
Lenzma <fluegel@dkrz.de>
2023-02-01 13:25:47 +0100

    set sextant to daily cronjob.

--- mdingestion/community/sextant.py
+++ mdingestion/community/sextant.py
@@ -12,7 +12,7 @@ class Sextant(Repository):
     SERVICE_TYPE = ServiceType.CSW
     PRODUCTIVE = True
     DATE = '2023-01-30'
-    CRON_DAILY = False
+    CRON_DAILY = True
     LOGO = "http://b2find.dkrz.de/images/communities/sextant_logo.png"
     DESCRIPTION = "Sextant is a marine and coastal geographic data infrastructure. It is operated by Scientific Information Systems for the Sea (SISMER) of Ifremer. Sextant aims to document, disseminate and promote a catalog of data related to the marine environment. For Ifremer's laboratories and partners, as well as for national and European actors working in the marine and coastal field, Sextant provides tools that promote and facilitate the archiving, consultation and availability of these geographical data. Data published by Sextant are available free or restricted. They can be used in accordance with the terms of the Creative Commons license selected by the author of data. Sextant infrastructure and the technologies used are in line with the implementation of the INSPIRE Directive and make it possible to follow the Open Data approach. Some data set published by Sextant has a DOI which enables it to be cited in a publication in a reliable and sustainable way. The long-term preservation of data filed in Sextant is ensured by Ifremer infrastructure."
 
