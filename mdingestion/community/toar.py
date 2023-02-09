68bc4f35e7b44b046220945097118c403441eca9
seolih <martens@dkrz.de>
2023-02-09 09:51:43 +0100

    new mapfiles for TOAR and Edmond

--- mdingestion/community/toar.py
+++ mdingestion/community/toar.py
@@ -4,8 +4,9 @@ from ..service_types import SchemaType, ServiceType
 
 class Toar(Repository):
     IDENTIFIER = 'toar'
-    URL = 'https://b2share.fz-juelich.de/api/oai2d'
-    SCHEMA = SchemaType.DublinCore
+    # URL = 'https://b2share.fz-juelich.de/api/oai2d'
+    URL = 'https://b2share-testing.fz-juelich.de/api/oai2d'
+    SCHEMA = SchemaType.Eudatcore
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'eudatcore'
     OAI_SET = '26e8202d-7094-412d-be28-7e64bf6ac77f'
