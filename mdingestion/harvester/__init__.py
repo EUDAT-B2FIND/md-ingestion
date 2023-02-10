f17bc330dbe02fe2d62db79a653fe84712a2605c
seolih <martens@dkrz.de>
2023-02-10 18:26:55 +0100

    pep8

--- mdingestion/harvester/__init__.py
+++ mdingestion/harvester/__init__.py
@@ -35,11 +35,9 @@ def harvester(repo,
             outdir=outdir,
             verify=verify)
     elif service_type == ServiceType.OAI:
-        harvester = OAIHarvester(repo, url, oai_metadata_prefix, oai_set, fromdate, clean, limit, outdir, verify,
-                                 username, password)
+        harvester = OAIHarvester(repo, url, oai_metadata_prefix, oai_set, fromdate, clean, limit, outdir, verify, username, password)
     elif service_type == ServiceType.OAI_IVOA:
-        harvester = OAIHarvesterIvoa(repo, url, oai_metadata_prefix, oai_set, fromdate, clean, limit, outdir, verify,
-                                 username, password)    
+        harvester = OAIHarvesterIvoa(repo, url, oai_metadata_prefix, oai_set, fromdate, clean, limit, outdir, verify, username, password)
     elif service_type == ServiceType.CSW:
         harvester = CSWHarvester(repo, url, schema, fromdate, clean, limit, outdir, verify)
     elif service_type == ServiceType.ArcGIS:
