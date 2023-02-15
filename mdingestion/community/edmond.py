a48f7999f146b6024c5a6bf7d27c0a505d4d855e
seolih <martens@dkrz.de>
2023-02-15 17:45:31 +0100

    changes in mapfile for edmond

--- mdingestion/community/edmond.py
+++ mdingestion/community/edmond.py
@@ -1,7 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 15 17:29:01 2023
+from .base import Repository
+from ..service_types import SchemaType, ServiceType
 
-@author: seolih
-"""
+
+class EdmondDatacite(Repository):
+    IDENTIFIER = 'edmond'
+    URL = 'https://oai.datacite.org/oai'
+    SCHEMA = SchemaType.DataCite
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'oai_datacite'
+    OAI_SET = 'TIB.MPDL'
+    PRODUCTIVE = False
+    DATE = '2023-01-10'
+    CRON_DAILY = False
+    LOGO = "https://edmond.mpdl.mpg.de/logos/navbar/logo_for_bright.png;jsessionid=5358848a5a7c5bebb3eb15bec784"
+    DESCRIPTION = """Edmond is a research data repository for Max Planck researchers. It is the place to store completed datasets of research data with open access. Edmond serves the publication of research data from all disciplines and offers scientists the ability to create citable research objects. https://edmond.mpdl.mpg.de/"""
+    REPOSITORY_ID = 'http://doi.org/10.17616/R3N33V'
+    REPOSITORY_NAME = 'EDMOND'
+
+
+    def update(self, doc):
+        doc.publisher = 'B2Find'
