f75503700e5e1340d1a3ec4e4a680629b0d8b7c6
seolih <martens@dkrz.de>
2023-02-16 19:39:31 +0100

    fix place, not working

--- mdingestion/community/edmond.py
+++ mdingestion/community/edmond.py
@@ -25,7 +25,7 @@ class Edmond(Repository):
         doc.creator = self.find('authors')
         doc.title = self.find('name')
         doc.keywords = self.find('keywords')
-        # doc.rights = 'whatever'
+        doc.rights = 'CC BY 4.0'
         doc.version = self.find('majorVersion')
         doc.resource_type = 'Dataset'
         doc.funding_reference = self.funding(doc)
