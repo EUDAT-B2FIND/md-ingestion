9b44a24d3c680cfc642ffbaa311531a4d476adb5
Lenzma <fluegel@dkrz.de>
2023-03-02 09:04:37 +0100

    added sets to mapfile.

--- mdingestion/community/hzb.py
+++ mdingestion/community/hzb.py
@@ -3,12 +3,12 @@ from ..service_types import SchemaType, ServiceType
 
 
 class HZBDatacite(Repository):
-    IDENTIFIER = 'hzb'
+    NAME = 'hzb'
     URL = 'https://data.helmholtz-berlin.de/oaipmh/request'
     SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_datacite'
-    OAI_SET = 'pub'
+#    OAI_SET = 'pub'
     PRODUCTIVE = False
     DATE = ''
     CRON_DAILY = False
@@ -16,3 +16,20 @@ class HZBDatacite(Repository):
     DESCRIPTION = ""
     REPOSITORY_ID = ''
     REPOSITORY_NAME = ''
+
+
+class HZBpub(HZBDatacite):
+    IDENTIFIER = 'hzb_pub'
+    OAI_SET = 'pub'
+
+
+class HZBinv(HZBDatacite):
+    IDENTIFIER = 'hzb_inv'
+    OAI_SET = 'raw_inv'
+
+    def update(self, doc):
+        doc.instrument = self.find('') # <relatedItem relatedItemType="Instrument" relationType="IsCompiledBy">
+              <relatedItemIdentifier relatedItemIdentifierType="DOI">10.5442/NI000001</relatedItemIdentifier>
+              <titles>
+                <title>E2 - Flat-Cone Diffractometer</title>
+              </titles>
