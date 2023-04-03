f9966f5a50704a80a26fc9a470a6c7df00690807
seolih <martens@dkrz.de>
2023-04-03 10:01:40 +0000

    new file for GROUP = panoscexpands

---
+++ mdingestion/community/panoscexpands.py
@@ -0,0 +1,11 @@
+from .base import Repository
+
+
+class BasePanoscExpands(Repository):
+    GROUP = 'pans'
+    GROUP_TITLE = 'Photon and Neutron Sciences'
+    PRODUCTIVE = True
+    DATE = '2022-07-14'
+    DESCRIPTION = """The Photon and Neutron Open Science Cloud PaNOSC is a European project for making FAIR data a reality in 6 European Research Infrastructures (RIs), developing and providing services for scientific data and connecting these to the European Open Science Cloud (EOSC). ExPaNDS is the European Open Science Cloud (EOSC) Photon and Neutron Data Service. The ambitious ExPaNDS project is a collaboration between 10 national Photon and Neutron Research Infrastructures (PaN RIs) as well as EGI. The project aims to deliver standardised, interoperable, and integrated data sources and data analysis services for Photon and Neutron facilities."""
+    LOGO = """https://www.fairsfair.eu/sites/default/files/styles/responsive/public/exd.jpg?itok=vdzRTYVO"""
+    LINK = 'https://www.panosc.eu/, https://expands.eu/'
