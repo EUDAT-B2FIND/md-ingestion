7f7de57c737d25465744fff3fff58abe7d2a57da
Lenzma <fluegel@dkrz.de>
2023-04-12 08:07:18 +0200

    changed harvesting schema from dc to datacite.

--- mdingestion/community/cora.py
+++ mdingestion/community/cora.py
@@ -2,14 +2,12 @@ from .base import Repository
 from ..service_types import SchemaType, ServiceType
 
 
-class CORADC(Repository):
+class CORADatacite(Repository):
     IDENTIFIER = 'cora'
     TITLE = 'CORA.RDR'
     URL = 'https://dataverse.csuc.cat/oai'
-#    SCHEMA = SchemaType.DublinCore
     SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
-#    OAI_METADATA_PREFIX = 'oai_dc'
     OAI_METADATA_PREFIX = 'oai_datacite'
     OAI_SET = 'OPENAIRE'
     PRODUCTIVE = False
@@ -21,6 +19,3 @@ class CORADC(Repository):
     DESCRIPTION = """
     The CORA.Repositori de Dades de Recerca (CORA.RDR) is a repository of open, curated and FAIR data that covers all academic disciplines. CORA.RDR is a shared service provided by participating Catalan institutions (Universities and CERCA Research Centers). The repository is managed by the CSUC and technical infrastructure is based on Dataverse.
     """
-
-#    def update(self, doc):
-#        doc.publisher = 'B2Find'
