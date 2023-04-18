1998e120d0ca2e2c368f422f65b7ec6e4f4eafb7
Lenzma <fluegel@dkrz.de>
2023-04-18 13:43:08 +0200

    added fix for missing temporal coverage.

--- mdingestion/community/slks.py
+++ mdingestion/community/slks.py
@@ -28,24 +28,27 @@ class Slks(BaseNordicar):
         doc.temporal_coverage = self.temporal_coverage(doc)
 
     def temporal_coverage(self, doc):
-        temporal = self.find('temporal')
-        period = temporal[0]
-        year = temporal[1]
-        from_year = int(year.split(',')[0])
-        doc.temporal_coverage_begin_date = f"{from_year}"
-        if from_year < 0:
-            from_year = f"{abs(from_year)} BC"
-        else:
-            from_year = f"{from_year} AD"
-        to_year = int(year.split(',')[1])
-        doc.temporal_coverage_end_date = f"{to_year}"
-        if to_year < 0:
-            to_year = f"{abs(to_year)} BC"
-        else:
-            to_year = f"{to_year} AD"
-        if len(temporal) >= 3:
-            main_period = temporal[2]
-            coverage = f"{from_year} - {to_year}; {period}; {main_period}"
-        else:
-            coverage = f"{from_year} - {to_year}; {period}"
+        try:
+            temporal = self.find('temporal')
+            period = temporal[0]
+            year = temporal[1]
+            from_year = int(year.split(',')[0])
+            doc.temporal_coverage_begin_date = f"{from_year}"
+            if from_year < 0:
+                from_year = f"{abs(from_year)} BC"
+            else:
+                from_year = f"{from_year} AD"
+            to_year = int(year.split(',')[1])
+            doc.temporal_coverage_end_date = f"{to_year}"
+            if to_year < 0:
+                to_year = f"{abs(to_year)} BC"
+            else:
+                to_year = f"{to_year} AD"
+            if len(temporal) >= 3:
+                main_period = temporal[2]
+                coverage = f"{from_year} - {to_year}; {period}; {main_period}"
+            else:
+                coverage = f"{from_year} - {to_year}; {period}"
+        except Exception:
+            coverage = None
         return coverage
