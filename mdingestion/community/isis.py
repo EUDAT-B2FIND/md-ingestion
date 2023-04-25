760f4a87635c1375fb53b74e4a7d54b1508b0fe8
seolih <martens@dkrz.de>
2023-04-25 11:55:06 +0000

    new set with DOI on productive endpoint

--- mdingestion/community/isis.py
+++ mdingestion/community/isis.py
@@ -4,8 +4,8 @@ from ..service_types import SchemaType, ServiceType
 
 class ISISDatacite(Repository):
     IDENTIFIER = 'isis'
-    URL = 'https://icat-dev.isis.stfc.ac.uk/oaipmh/request'
-#   URL = 'https://icatisis.esc.rl.ac.uk/oaipmh/request'
+#   URL = 'https://icat-dev.isis.stfc.ac.uk/oaipmh/request'
+    URL = 'https://icatisis.esc.rl.ac.uk/oaipmh/request'
     SCHEMA = SchemaType.DataCite
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'oai_datacite'
@@ -13,9 +13,10 @@ class ISISDatacite(Repository):
     PRODUCTIVE = False
     REPOSITORY_ID = 're3data:r3d100014115'
     REPOSITORY_NAME = 'ISIS DataGateway'
-#    DATE = '2023-01.17'
-#    LOGO = "http://b2find.dkrz.de/images/communities/darus_logo.png"
-#    DESCRIPTION = """ some description here """
+    DATE = '2023-04-25'
+    LOGO = "https://users.facilities.rl.ac.uk/auth/Images/Isis-Logo.png"
+    LINKS = "https://www.isis.stfc.ac.uk/Pages/home.aspx", "https://data.isis.stfc.ac.uk/datagateway"
+    DESCRIPTION = """ISIS Neutron and Muon Source is a world-leading centre for research at the STFC Rutherford Appleton Laboratory. Our neutron and muon instruments give unique insights into the properties of materials on the atomic scale. You can browse, explore and visualise experimental data via the DataGateway."""
 
     def update(self, doc):
         doc.discipline = self.discipline(doc, 'Photon- and Neutron Geosciences')
