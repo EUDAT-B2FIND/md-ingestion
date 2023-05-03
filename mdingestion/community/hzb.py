011816cb88621102aceb8de7ee1099ee24c240ee
Lenzma <fluegel@dkrz.de>
2023-05-03 08:05:22 +0200

    updated mapfile.

--- mdingestion/community/hzb.py
+++ mdingestion/community/hzb.py
@@ -14,7 +14,7 @@ class HZBDatacite(BasePanoscExpands):
     CRON_DAILY = False
     LOGO = "https://www.helmholtz-berlin.de/media/design/logo/hzb-logo.svg"
     DESCRIPTION = """Helmholtz-Zentrum Berlin für Materialien und Energie (HZB) strives to achieve a climate neutral society through science and innovation. Scientists are developing and optimising efficient and cost-effective materials amongst others for solar cells, batteries and catalysts. Therefore, we run state-of-the art labs and BESSY II light source, which delivers intensely bright light, soft X-rays in particular. Researchers are using this special light to study the structure and function of energy and quantum materials. BESSY II has an annual average of 2700 user visits from 28 countries. HZB scientists are also conducting research on new concepts for accelerator-based light sources."""
-    REPOSITORY_ID = 'https://ror.org/02aj13c28'
+    REPOSITORY_ID = 'ror:02aj13c28'
     REPOSITORY_NAME = 'Helmholtz-Zentrum Berlin für Materialien und Energie'
 
 
