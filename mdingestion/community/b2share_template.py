ab7fbc31379b93c836142e2fe47c909ddbb74fc3
seolih <martens@dkrz.de>
2023-05-10 18:33:13 +0000

    new b2share_template for Community integration

---
+++ mdingestion/community/b2share_template.py
@@ -0,0 +1,33 @@
+from .base import Repository
+from ..service_types import SchemaType, ServiceType
+
+
+class WhateverinEudatcore(Repository):
+    IDENTIFIER = 'example'  # if possible please avoid too long names and '-' or '+' because we never know how it looks like in the browser then, must be the same name as the mapfile, e.g. example.py
+    TITLE = 'ExAmple'   # the Name of the Repository on the Web GUI
+    GROUP = 'b2share'   # like this because that attaches a tag to each record of this example community
+    URL = 'https://b2share.eudat.eu/api/oai2d'  
+    SCHEMA = SchemaType.Eudatcore
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'eudatcore'
+    OAI_SET = 'veeeery-important-string-here'   # Whatever Set from CSC
+    PRODUCTIVE = False  # as long as it is not on productive machine...
+    DATE = '2023-01-23' # will be done by B2FIND when it is productive
+    REPOSITORY_ID = ''  # very important because this information is transferred to OpenAIRE; allowed values only IDs from re3data or fairsharing, example: REPOSITORY_ID = 're3data:r3d100013171'
+    REPOSITORY_NAME = ''    # same as above, example: REPOSITORY_NAME = 'DaRUS'
+    CRON_DAILY = False  
+    LINK = ""   # important for creating the Repository CKAN Web GUI
+    LOGO = ""   # important for creating the Repository CKAN Web GUI
+    DESCRIPTION = """"""    # important for creating the Repository CKAN Web GUI
+
+    def update(self, doc):  # all update methods are default for *if not*, so if the information is given we take it but if there is no information we have a default
+        if not doc.publisher:
+            doc.publisher = 'EUDAT B2SHARE' # because publisher = mandatory element in B2FIND
+        if not doc.publication_year:
+            doc.publication_year = self.find('header.datestamp')    # because publication_year = mandatory element in B2FIND
+        if not doc.resource_type:
+            doc.resource_type = 'Dataset'   # because otherwise records don´t go to OpenAIRE...
+        if not doc.discipline:
+            doc.discipline = 'Social/Natural Sciences' # because it´s good for the search, only for thematic communities
+        if not doc.contact:
+            doc.contact = 'somecrazyemail@coolrepo.eu, www.anyhelpdesk.eu' # because that could be useful, don´t use when there is none
--- mdingestion/community/bbmri.py
+++ mdingestion/community/bbmri.py
@@ -7,16 +7,17 @@ class BbmriDublincore(Repository):
     TITLE = 'BBMRI'
     GROUP = 'b2share'
     URL = 'https://b2share.eudat.eu/api/oai2d'
-    SCHEMA = SchemaType.DublinCore
+    SCHEMA = SchemaType.Eudatcore
     SERVICE_TYPE = ServiceType.OAI
-    OAI_METADATA_PREFIX = 'oai_dc'
+    OAI_METADATA_PREFIX = 'eudatcore'
     OAI_SET = '99916f6f-9a2c-4feb-a342-6552ac7f1529'  # BBMRI Set from CSC
     PRODUCTIVE = True
-    DATE = '2023-01-23'
+    DATE = '2023-05-10'
     REPOSITORY_ID = ''
     REPOSITORY_NAME = ''
     CRON_DAILY = False
     LOGO = ""
+    LINK = ""
     DESCRIPTION = """The main vision of the project was to increase the scientific excellence and efficacy of European research in the biomedical sciences with the aim of expanding and securing competitiveness in a global context as well as attracting investments in pharmaceutical and biomedical research facilities. BBMRI reached the goals forecasted by creating a pan-European research infrastructure and by developing biobanks and biomolecular resources. Furthermore, BBMRI provided European researchers with effective access to resources and facilities, along with common services that include a joint IT service and scientific, technical, ethical, and legal expertise. Through a European framework, researchers could gain support at all stages of the biomedical R&D process. The preparatory phase of BBMRI came to an end in January 2011. And in its last year, BBMRI grew into a 54-member consortium with more than 225 associated organisations from over 30 countries, making it one of the largest research infrastructure projects in Europe. It transformed again to become a more permanent initiative, now BBMRI-ERIC."""
 
     def update(self, doc):
@@ -25,4 +26,4 @@ class BbmriDublincore(Repository):
         if not doc.publication_year:
             doc.publication_year = self.find('header.datestamp')
         doc.discipline = 'Biological and Medical Research'
-        #    doc.resource_type = 'Dataset'
+    #   doc.resource_type = 'Dataset'
