e6fe4ce0e0cc06b5143faa1c96e8212fb2983824
seolih <martens@dkrz.de>
2023-06-12 15:05:55 +0000

    update on re3data entry

--- mdingestion/community/icos.py
+++ mdingestion/community/icos.py
@@ -13,5 +13,8 @@ class IcosDatacite(Repository):
     DATE = '2023-01-31'
     CRON_DAILY = False
     LOGO = "https://www.icos-cp.eu/sites/default/files/ICOS-logo.svg"
-    DESCRIPTION = """ Integrated Carbon Observation System (ICOS https://www.icos-cp.eu/data-services/about-data-portal) is a pan-European research infrastructure which provides harmonised and high-precision scientific data on carbon cycle and greenhouse gas budget and perturbations.
-    """
+    LINK = ''
+    DESCRIPTION = """ICOS Carbon Portal is the data portal of the Integrated Carbon Observation System. It provides observational data from the state of the carbon cycle in Europe and the world. The Carbon Portal is the data center of the ICOS infrastructure. ICOS will collect greenhouse gas concentration and fluxes observations from three separate networks, all these observations are carried out to support research to help us understand how the Earth’s greenhouse gas balance works, because there are still many and large uncertainties!"""
+    LINK = 'https://www.icos-cp.eu/'
+    REPOSITORY_ID = 're3data:r3d100012203'
+    REPOSITORY_NAME = 'ICOS Carbon Portal'
