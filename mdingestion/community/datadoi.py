6c4334b612e29e87d17d9a0709cc1ee41cba2b00
seolih <martens@dkrz.de>
2023-06-12 14:46:52 +0000

    changes in mapfile

--- mdingestion/community/datadoi.py
+++ mdingestion/community/datadoi.py
@@ -16,9 +16,8 @@ class DatadoiDublincore(Repository):
     REPOSITORY_ID = ''
     REPOSITORY_NAME = ''
     LOGO = ""
-    DESCRIPTION = """
-    DublinCore Template with SEANOE
-    """
+    DESCRIPTION = """The mission of DataDOI is to offer preservation and dissemination of Estonian research data from various fields. The main goal is to ensure that research data produced by Estonian researchers is findable, accessible, interoperable and reusable. Through preserving open research data DataDOI enriches academic quality and collaboration, supports innovative developments and supports overall use of scientific materials. DataDOI is an institutional research data repository managed by University of Tartu Library. DataDOI gathers all fields of research data and stands for encouraging open science and FAIR (Findable, Accessible, Interoperable, Reusable) principles. DataDOI is made for long-term preservation of research data. Each dataset is given a DOI (Digital Object Identifier) through DataCite Estonia Concortium."""
+    LINK = 'https://datadoi.ee/'
 
     def update(self, doc):
         doc.publisher = 'B2Find'
