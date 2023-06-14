722f40bdd060912304be6bc063a1e1f3fee979ea
Lenzma <fluegel@dkrz.de>
2023-06-14 12:37:50 +0200

    specified creator mapping.

--- mdingestion/community/wdcc.py
+++ mdingestion/community/wdcc.py
@@ -21,13 +21,31 @@ class WDCCIso(Repository):
 
     def update(self, doc):
         doc.doi = self.find_doi('MD_Identifier.CharacterString')
-        doc.creator = self.find('citedResponsibleParty.CI_ResponsibleParty.individualName.CharacterString')
+        doc.creator = self._creator(doc)
         doc.contact = self.find('CI_Contact.linkage')
         doc.discipline = self.discipline(doc, 'Earth System Research')
         doc.publisher = 'World Data Center for Climate (WDCC)'
         doc.version = self.find('MD_DataIdentification.citation.edition')
         doc.format = self.find('MD_Format')
         doc.rights = self._rights(doc)
+        doc.funding_reference = self.find('MD_DataIdentification.supplementalInformation.CharacterString')
+
+    def _creator(self,doc):
+        selected_creators = []
+        try:
+            creators = self.reader.parser.doc.MD_DataIdentification.CI_Citation.citedResponsibleParty.find_all('CI_ResponsibleParty')
+            for creator in creators:
+                try:
+                    name = creator.individualName.CharacterString.text
+                    codetype = creator.role.CI_RoleCode['codeListValue']
+                    if codetype in ['owner', 'originator', 'pointOfContact', 'principalInvestigator', 'author']:
+                        selected_creators.append(name)
+                except Exception:
+                    pass
+        except Exception:
+            pass
+        return selected_creators
+
 
     def _rights(self, doc):
         if not doc.rights:
