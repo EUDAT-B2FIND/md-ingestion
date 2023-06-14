20d34386d97a76be8c29b60d4e52a21dc1b0f8c6
Lenzma <fluegel@dkrz.de>
2023-06-14 16:46:23 +0200

    added workaround for reference system 0-360 degrees in spatial coverage.

--- mdingestion/reader/iso19139.py
+++ mdingestion/reader/iso19139.py
@@ -61,9 +61,9 @@ class ISO19139Reader(XMLReader):
                 # west = convert_to_lon_180(west)
                 east = format_value(self.find('EX_GeographicBoundingBox.eastBoundLongitude'), type='float', one=True)
                 # east = convert_to_lon_180(east)
-                if int(west) == 0 and int(east) == 360:
-                    west = -180.0
-                    east = 180.0
+                if east > 180:
+                    west = west - 180.0
+                    east = east - 180.0
                 south = format_value(self.find('EX_GeographicBoundingBox.southBoundLatitude'), type='float', one=True)
                 north = format_value(self.find('EX_GeographicBoundingBox.northBoundLatitude'), type='float', one=True)
                 # bbox: minx=west, miny=south, maxx=east, maxy=north
