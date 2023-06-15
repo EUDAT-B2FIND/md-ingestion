5d1c786d7da1f111d0cca57c687ea4b4c74ef257
seolih <martens@dkrz.de>
2023-06-15 13:17:14 +0000

    changes for repoID and Name for B2SHARE / FZJ

--- mdingestion/community/eudat.py
+++ mdingestion/community/eudat.py
@@ -7,8 +7,6 @@ class BaseEudat(Repository):
     NAME = 'eudat'
     PRODUCTIVE = True
     DATE = '2023-01-23'
-    REPOSITORY_ID = 're3data:r3d100011395'
-    REPOSITORY_NAME = 'EUDAT'
 
     def update(self, doc):
         if not doc.publication_year:
@@ -23,6 +21,8 @@ class EudatCsc(BaseEudat):
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'eudatcore'
     OAI_SET = 'e9b9792e-79fb-4b07-b6b4-b9c2bd06d095'  # EUDAT Set from CSC
+    EPOSITORY_ID = 're3data:r3d100011394'
+    REPOSITORY_NAME = 'B2SHARE'
 
 
 class EudatFzj(BaseEudat):
@@ -33,3 +33,5 @@ class EudatFzj(BaseEudat):
     SERVICE_TYPE = ServiceType.OAI
     OAI_METADATA_PREFIX = 'eudatcore'
     OAI_SET = 'e9b9792e-79fb-4b07-b6b4-b9c2bd06d095'  # EUDAT Set from FZJ
+    EPOSITORY_ID = 're3data:r3d100013118'
+    REPOSITORY_NAME = 'B2SHARE Server Forschungszentrum Jülich'
