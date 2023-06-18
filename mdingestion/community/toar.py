f7e9d27ebf96a6c54772bf58cea8ff2ec1eb39aa
seolih <martens@dkrz.de>
2023-06-18 13:48:14 +0000

    changes for TOAR

--- mdingestion/community/toar.py
+++ mdingestion/community/toar.py
@@ -13,11 +13,11 @@ class Toar(Repository):
     OAI_SET = '381a24f1-18d4-405d-af36-c76ba199a754'
     PRODUCTIVE = False
     DATE = '2023-02-09'
+    REPOSITORY_ID = 're3data:r3d100013118'
+    REPOSITORY_NAME = 'B2SHARE Server Forschungszentrum Jülich'
     LOGO = "https://igacproject.org/sites/default/files/2019-11/TOAR_Logo.png"
     LINK = 'https://igacproject.org/activities/TOAR'
     DESCRIPTION = """The "Tropospheric Ozone Assessment Report" (TOAR) has been created by ~220 scientists from 36 countries to produce the first tropospheric ozone assessment report based on the peer-reviewed literature and new analyses, and to generate easily accessible, documented data on ozone exposure and dose metrics at hundreds of measurement sites around the world (urban and non-urban), freely accessible for research on the global-scale impact of ozone on climate, human health and crop/ecosystem productivity."""
-    EPOSITORY_ID = 're3data:r3d100013118'
-    REPOSITORY_NAME = 'B2SHARE Server Forschungszentrum Jülich'
 
     def update(self, doc):
         if not doc.publisher:
