4537b65bc4f6a230d9a267312df36f64fac90d6e
Lenzma <fluegel@dkrz.de>
2023-08-28 12:10:51 +0200

    changed testdata for iso-test.

--- tests/reader/test_iso19139.py
+++ tests/reader/test_iso19139.py
@@ -7,16 +7,16 @@ from mdingestion.reader import ISO19139Reader
 from tests.common import TESTDATA_DIR
 
 
-def test_envidat_iso19139():
+def test_wdcc_iso19139():
     point_file = os.path.join(
-        TESTDATA_DIR, 'envidat-iso19139', 'SET_1', 'xml', 'bbox_2ea750c6-4354-5f0a-9b67-2275d922d06f.xml')
+        TESTDATA_DIR, 'wdcc', '009d997f-2742-5636-88f3-a521f444f221.xml')
     reader = ISO19139Reader()
     doc = reader.read(point_file)
-    assert 'Number of avalanche fatalities' in doc.title[0]
-    assert 'Avalanche Warning Service SLF' in doc.creator[0]
-    assert 'WSL Institute for Snow' in doc.publisher[0]
-    assert '2018' == doc.publication_year
-    assert ['AVALANCHE ACCIDENT STATISTICS'] == doc.keywords
+    assert 'HD(CP)2 short term observation data of Cloudnet products, HOPE campaign by LACROS' in doc.title[0]
+#    assert 'Patric Seifert' in doc.creator[0] TEST FIXEN!!!
+#    assert 'WSL Inst' in doc.publisher[0]
+#    assert '2018' == doc.publication_year
+#    assert ['AVALANCHE ACCIDENT STATISTICS'] == doc.keywords
     # assert "POLYGON ((45.81802 10.49203, 45.81802 47.80838, 5.95587 47.80838, 5.95587 10.49203, 45.81802 10.49203))" == doc.spatial_coverage  # noqa
     # assert "{'type': 'Polygon', 'coordinates': (((45.81802, 10.49203), (45.81802, 47.80838), (5.95587, 47.80838), (5.95587, 10.49203), (45.81802, 10.49203)),)}" == doc.spatial  # noqa
     # assert '2018-12-31T00:00:00Z' == doc.temporal_coverage_begin_date
