7a7a04954f8bd7d5079184d86bf52e07779327d3
Lenzma <fluegel@dkrz.de>
2024-04-22 17:03:01 +0200

    generalized method "instruments" for both hzb_pub and hzb_inv.

--- mdingestion/community/hzb.py
+++ mdingestion/community/hzb.py
@@ -17,11 +17,29 @@ class HZBDatacite(BasePanoscExpands):
     REPOSITORY_ID = 'opendoar:3680'
     REPOSITORY_NAME = 'Helmholtz-Zentrum Berlin für Materialien und Energie'
 
+    def instrument(self, doc):
+        result = []
+        insts = self.reader.parser.doc.find_all('relatedItem', relatedItemType="Instrument")
+        for inst in insts:
+            title = inst.titles.title.text
+            try:
+                ident = inst.relatedItemIdentifier.text
+                ident_type = inst.relatedItemIdentifier['relatedItemIdentifierType']
+                if ident_type == 'DOI':
+                    result.append(f'{title}, https://doi.org/{ident}')
+                elif ident_type == 'PID':
+                    result.append(f'{title}, https://hdl.handle.net/{ident}')
+            except Exception as err:
+                result.append(title)
+        return result
+
 
 class HZBpub(HZBDatacite):
     IDENTIFIER = 'hzb_pub'
     OAI_SET = 'pub'
 
+    def update(self, doc):
+        doc.instrument = self.instrument(doc)
 
 class HZBinv(HZBDatacite):
     IDENTIFIER = 'hzb_inv'
@@ -37,22 +55,6 @@ class HZBinv(HZBDatacite):
         pids = self.find('resource.identifier')
         for pid in pids:
             result.append(f'https://hdl.handle.net/{pid}')
-        return result
-
-    def instrument(self, doc):
-        result = []
-        insts = self.reader.parser.doc.find_all('relatedItem', relatedItemType="Instrument")
-        for inst in insts:
-            title = inst.titles.title.text
-            try:
-                ident = inst.relatedItemIdentifier.text
-                ident_type = inst.relatedItemIdentifier['relatedItemIdentifierType']
-                if ident_type == 'DOI':
-                    result.append(f'{title}, https://doi.org/{ident}')
-                elif ident_type == 'PID':
-                    result.append(f'{title}, https://hdl.handle.net/{ident}')
-            except Exception as err:
-                result.append(title)
         return result
 
     def creator(self, doc):
