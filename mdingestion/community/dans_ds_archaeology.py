5550731de12437b523845903fe9d63312e680ec1
Lenzma <fluegel@dkrz.de>
2024-09-03 14:27:36 +0200

    cleaned up mapfiles.

--- mdingestion/community/dans_ds_archaeology.py
+++ mdingestion/community/dans_ds_archaeology.py
@@ -1,19 +1,10 @@
-from .base import Repository
-from ..service_types import SchemaType, ServiceType
-from ..format import format_value
+from .dans import BaseDans
 
 
-class Ds_Archaeology(BaseDans):
+class DSArchaeology(BaseDans):
     IDENTIFIER = 'ds_archaeology'
     TITLE = 'DANS Data Station Archaeology'
-    GROUP = 'dans'
     URL = 'https://archaeology.datastations.nl/oai'
-    PRODUCTIVE = True
-    DATE = '2024-08-28'
     DESCRIPTION = 'A domain-specific repository for Archaeology and related data, primarily aimed at the Netherlands, but not exclusively.'
-    LOGO = ''
-    SCHEMA = SchemaType.DataCite
-    SERVICE_TYPE = ServiceType.OAI
-    OAI_METADATA_PREFIX = 'oai_datacite'
     REPOSITORY_ID = 're3data:r3d100014005'
     REPOSITORY_NAME = 'DANS: Data Station Archaeology'
--- mdingestion/community/dans_ds_lifesciences.py
+++ mdingestion/community/dans_ds_lifesciences.py
@@ -1,19 +1,10 @@
-from .base import Repository
-from ..service_types import SchemaType, ServiceType
-from ..format import format_value
+from .dans import BaseDans
 
 
-class Ds_LifeSciences(BaseDans):
+class DSLifeSciences(BaseDans):
     IDENTIFIER = 'ds_lifesciences'
     TITLE = 'DANS Data Station Life Sciences'
-    GROUP = 'dans'
     URL = 'https://lifesciences.datastations.nl/oai'
-    PRODUCTIVE = True
-    DATE = '2024-08-28'
     DESCRIPTION = 'A domain-specific repository for the Life Sciences, covering the health, medical as well as the green life sciences. The repository services are primarily aimed at the Netherlands, but not exclusively.'
-    LOGO = ''
-    SCHEMA = SchemaType.DataCite
-    SERVICE_TYPE = ServiceType.OAI
-    OAI_METADATA_PREFIX = 'oai_datacite'
     REPOSITORY_ID = 're3data:r3d100014304'
     REPOSITORY_NAME = 'DANS Data Station Life Sciences'
