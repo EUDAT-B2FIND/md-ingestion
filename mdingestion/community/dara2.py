66e2342f2be92b3d59548a76232cf5fe1c9c889d
Lenzma <fluegel@dkrz.de>
2025-05-08 16:38:22 +0200

    workde on mapfile.

--- mdingestion/community/dara2.py
+++ mdingestion/community/dara2.py
@@ -17,33 +17,49 @@ class DARADatacite(Repository):
     LOGO = ""
     DESCRIPTION = ""
 
+
     def update(self, doc):
         doc.discipline = ['Social Sciences']
-        doc.doi = self.find('attributes.doi')
-        doc.description = self._find('Abstract')
-        doc.source = self._find('OnlineResourceUrl')
-        doc.publication_year = self._find('Last_Update')
-        doc.contributor = self._find('Organisations')
-        doc.contact = ['blue-cloud-support@maris.nl']
-        doc.title = self._find('Title') or self._find('Abstract')
-        doc.temporal_coverage_begin_date = self._find('Temporal_Extent_Begin')
-        doc.temporal_coverage_end_date = self._find('Temporal_Extent_End')
-        doc.geometry = self.geometry()
-        doc.instrument = self.instruments()
-        doc.keywords = self.keywords()
-        doc.publisher = self.publishers()
+        doc.doi = self.doi()
+        doc.creator = self.creator()
+        doc.description = self.description()
+        '''
+        doc.publication_year = self._find('publicationYear')
+        doc.rights = self._find('rights')
+        doc.contributor = self._find('contributors')
+        doc.funding_reference = self._find('fundingReferences')
+        doc.related_identifier = self._find('relatedIdentifiers')
+        doc.format = self._find('formats')
+        doc.size = self._find('sizes')
+        doc.resource_type = self._find('resourceTypeGeneral')
+        doc.version = self._find('version')
+        doc.title = self._find('title')
+        doc.language = self._find('language')
+        doc.keywords = self._find('subject')
+        doc.publisher = self._find('publisher')
+        doc.places = self._find('geoLocationPlace')
+        '''
+    @property
+    def jsondoc(self):
+        return self.reader.parser.doc
 
+    def doi(self):
+        try:
+            doi_ = self.jsondoc['attributes']['doi']
+        except Exception as e:
+            doi_ = None
+        return doi_
 
     def creator(self):
-        creators = []
-        for creator in self.parser.doc.find_all('creator'):
-            name = creator.creatorName.text
-            orcid_element = creator.find('nameIdentifier', nameIdentifierScheme="ORCID")
-            if orcid_element:
-                orcid = orcid_element.text
-                c = f"{name} (ORCID: {orcid})"
-            else:
-                c = name
-            creators.append(c)
-        return creators
+        try:
+            creator_ = [c['name'] for c in self.jsondoc['attributes']['creators']]
+        except Exception as e:
+            creator_ = None
+        return creator_
 
+    def description(self):
+        try:
+            description_ = [c['description'] for c in self.jsondoc['attributes']['descriptions']]
+        except Exception as e:
+            description_ = None
+        return description_
